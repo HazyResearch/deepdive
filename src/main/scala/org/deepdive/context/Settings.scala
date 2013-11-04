dd1577442b93c2a768a150742f687f0369a05e7a
Denny Britz <dennybritz@gmail.com>
2013-11-04 14:05:25 -0800

    Added specs for parsing

--- src/main/scala/org/deepdive/context/Settings.scala
+++ src/main/scala/org/deepdive/context/Settings.scala
@@ -5,33 +5,36 @@ import org.deepdive._
 import org.deepdive.context.parsing._
 import scala.collection.JavaConversions._
 
-class Settings(config: Config) {
+case class Connection(host: String, port: Int, db: String, user: String, password: String)
+case class Relation(name: String, schema: Map[String, String])
+case class EtlTask(relation: String, source: String)
+case class Extractor(name:String, outputRelation: String, inputQuery: String, udf: String, factor: Factor)
+case class Factor(name: String, func: FactorFunction, weight: String)
 
-  case class Relation(name: String, schema: Map[String, String])
-  case class EtlTask(relation: String, source: String)
-  case class Extractor(name:String, outputRelation: String, inputQuery: String, udf: String, factor: Factor)
-  case class Factor(name: String, func: FactorFunction, weight: String)
+class Settings(config: Config) {
 
   // Validations makes sure that the supplied config includes all the required settings.
   config.checkValid(ConfigFactory.defaultReference(), "deepdive")
 
   // Connection settings
-  val connectionHost = config.getString("deepdive.global.connection.host")
-  val connectionPort = config.getInt("deepdive.global.connection.port")
-  val connectionDb = config.getString("deepdive.global.connection.db")
-  val connectionUser = config.getString("deepdive.global.connection.user")
-  val connectionPassword = config.getString("deepdive.global.connection.password")
+  val connection = Connection(
+    config.getString("deepdive.global.connection.host"),
+    config.getInt("deepdive.global.connection.port"),
+    config.getString("deepdive.global.connection.db"),
+    config.getString("deepdive.global.connection.user"),
+    config.getString("deepdive.global.connection.password")
+  )
 
   // Schema Settings
   val relations = config.getObject("deepdive.relations").keySet().map { relationName =>
     val schema =  config.getObject(s"deepdive.relations.$relationName.schema").unwrapped
     Relation(relationName,schema.toMap.mapValues(_.toString))
-  }
+  }.toList
 
   val etlTasks = config.getObject("deepdive.ingest").keySet().map { relationName =>
     val source = config.getString(s"deepdive.ingest.$relationName.source")
     EtlTask(relationName, source)
-  }
+  }.toList
 
   val extractors = config.getObject("deepdive.extractions").keySet().map { extractorName =>
     val outputRelation = config.getString(s"deepdive.extractions.$extractorName.output_relation")
---
+++ src/test/scala/SettingsSpec.scala
@@ -0,0 +1,30 @@
+import com.typesafe.config._
+import org.deepdive.context._
+import org.scalatest._
+
+class SettingsSpec extends FunSpec {
+
+  describe("Settings") {
+
+    it("should parse a simple configuration file") {
+      val settings = new Settings(ConfigFactory.load("simple_config"))
+      
+      assert(settings.connection == Connection("localhost", 5432, "deepdive_test", 
+        "root", "password"))
+      
+      assert(settings.relations == List(
+        Relation("documents", Map[String,String]("varid" -> "Integer", 
+          "text" -> "Text", "meta" -> "Text")),
+        Relation("entities", Map[String,String]("varid" -> "Integer", 
+          "document_id" -> "Integer", "name" -> "String", "meta" -> "Text"))
+      ))
+
+      assert(settings.etlTasks == List(
+        EtlTask("documents", "data/documents.tsv")
+      ))
+
+    }
+
+  }
+
+}
