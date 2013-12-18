cfeb7f98e5be07c4f1c417b930892570ef08d8ce
Denny Britz <dennybritz@gmail.com>
2013-12-17 22:24:23 -0800

    test refactoring

---
+++ src/test/scala/unit/extraction/ExtractionManagerSpec.scala
@@ -0,0 +1,11 @@
+package org.deepdive.test.unit
+
+import org.scalatest._
+
+class ExtractionManagerSpec extends FunSpec with BeforeAndAfter  {
+
+  describe("Extraction Manager") {
+    it("should work")(pending)
+  }
+
+}
