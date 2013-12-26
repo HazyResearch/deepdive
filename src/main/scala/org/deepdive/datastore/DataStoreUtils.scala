87a8c51cd15495830bea5a2deaedee0cffd4b956
Denny Britz <dennybritz@gmail.com>
2013-12-26 14:00:13 -0800

    some comments

---
+++ src/main/scala/org/deepdive/datastore/DataStoreUtils.scala
@@ -0,0 +1,41 @@
+package org.deepdive.datastore
+
+import anorm._
+import org.deepdive.Logging
+import org.deepdive.settings._
+import spray.json._
+import spray.json.DefaultJsonProtocol._
+
+/* Utilities for working with data stores */
+object DataStoreUtils extends Logging {
+
+  type AnormSeq = Seq[(String, ParameterValue[_])]  
+
+  /* 
+   * Converts a JSON objects row to a object that can be handled by the Anorm library
+   * Example: ["John", 15, True] => AnormSeq(...)
+   */
+  implicit def jsonRowToAnormSeq(row: JsObject) : AnormSeq = {
+    jsonRowsToAnormSeq(List(row)).head
+  }
+
+  /* 
+   * Converts a multiple JSON objects rows to a object that can be handled by Anorm
+   * Example: [["John", 15, True], ...] => Seq[AnormSeq(...), ...]
+   */
+  implicit def jsonRowsToAnormSeq[T <% Iterable[JsObject]]
+    (rows: T): Seq[AnormSeq] = {
+    rows.map { row =>
+      row.fields.mapValues { 
+        case(JsNull) => toParameterValue(null)
+        case(x: JsString) => toParameterValue(x.value)
+        case(x: JsNumber) => toParameterValue(x.value.toLong)
+        case(x: JsBoolean) => toParameterValue(x.value)
+        case (value) => 
+          log.error("Found value of type ${value.getClass.name} which is not supported.")
+          toParameterValue(null)
+      }.toSeq
+    }.toSeq
+  }
+  
+}
