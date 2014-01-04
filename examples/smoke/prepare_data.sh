8d6ef2cfd5054e1233e806ba99e768ffe6930b00
Mikhail Sushkov <sushkovm@gmail.com>
2014-01-03 16:33:11 -0800

    Updating smoke example

--- examples/smoke/prepare_data.sh
+++ examples/smoke/prepare_data.sh
@@ -14,7 +14,7 @@ createdb deepdive_smoke
 psql -c "drop schema if exists public cascade; create schema public;" $DB_NAME
 
 psql -c "create table people(id bigserial primary key, name text, has_cancer boolean, smokes boolean);" $DB_NAME
-psql -c "create table friends(id1 bigserial references people(id), id2 bigserial references people(id), are_friends boolean, primary key(id1, id2));" $DB_NAME
+psql -c "create table friends(id bigserial primary key, person_id bigserial references people(id), friend_id bigserial references people(id));" $DB_NAME
 
 psql -c "insert into people(id, name, smokes) values (1, 'Anna', TRUE);" $DB_NAME
 psql -c "insert into people(id, name) values (2, 'Bob');" $DB_NAME
@@ -23,16 +23,14 @@ psql -c "insert into people(id, name) values(4, 'Frank');" $DB_NAME
 psql -c "insert into people(id, name) values(5, 'Gary');" $DB_NAME
 psql -c "insert into people(id, name) values(6, 'Helen');" $DB_NAME
 
-psql -c "insert into friends(id1, id2, are_friends) values(1, 2, TRUE);" $DB_NAME
-psql -c "insert into friends(id1, id2, are_friends) values(1, 3, TRUE);" $DB_NAME
-psql -c "insert into friends(id1, id2, are_friends) values(1, 4, TRUE);" $DB_NAME
-psql -c "insert into friends(id1, id2, are_friends) values(3, 4, TRUE);" $DB_NAME
-psql -c "insert into friends(id1, id2, are_friends) values(5, 6, TRUE);" $DB_NAME
-psql -c "insert into friends(id1, id2, are_friends) values(5, 4, FALSE);" $DB_NAME
+psql -c "insert into friends(person_id, friend_id) values(1, 2);" $DB_NAME
+psql -c "insert into friends(person_id, friend_id) values(1, 3);" $DB_NAME
+psql -c "insert into friends(person_id, friend_id) values(1, 4);" $DB_NAME
+psql -c "insert into friends(person_id, friend_id) values(3, 4);" $DB_NAME
+psql -c "insert into friends(person_id, friend_id) values(5, 6);" $DB_NAME
 
-psql -c "insert into friends(id1, id2, are_friends) values(2, 1, TRUE);" $DB_NAME
-psql -c "insert into friends(id1, id2, are_friends) values(3, 1, TRUE);" $DB_NAME
-psql -c "insert into friends(id1, id2, are_friends) values(4, 1, TRUE);" $DB_NAME
-psql -c "insert into friends(id1, id2, are_friends) values(4, 3, TRUE);" $DB_NAME
-psql -c "insert into friends(id1, id2, are_friends) values(6, 5, TRUE);" $DB_NAME
-psql -c "insert into friends(id1, id2, are_friends) values(4, 5, FALSE);" $DB_NAME
+psql -c "insert into friends(person_id, friend_id) values(2, 1);" $DB_NAME
+psql -c "insert into friends(person_id, friend_id) values(3, 1);" $DB_NAME
+psql -c "insert into friends(person_id, friend_id) values(4, 1);" $DB_NAME
+psql -c "insert into friends(person_id, friend_id) values(4, 3);" $DB_NAME
+psql -c "insert into friends(person_id, friend_id) values(6, 5);" $DB_NAME
