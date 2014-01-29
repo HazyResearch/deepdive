5eaa0620fbac158202e2a6f2f22855aeb931fce5
Denny Britz <dennybritz@gmail.com>
2014-01-29 10:35:02 -0800

    Add script to run test.

---
+++ test.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+# Set username and password
+export PGUSER=${PGUSER:-`whoami`}
+export PGPASSWORD=${PGPASSWORD:-}
+
+# Create test database
+dropdb deepdive_test
+createdb deepdive_test
+
+# Run the test
+SBT_OPTS="-Xmx2g" sbt "test"
+
+# Drop test database
+dropdb deepdive_test
