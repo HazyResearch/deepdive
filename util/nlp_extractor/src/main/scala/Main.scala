5516f838bfe997d032b51a6c3ffec07403ade53f
Denny Britz <dennybritz@gmail.com>
2014-02-02 11:50:11 -0800

    Add debug output to NLP parser

--- util/nlp_extractor/src/main/scala/Main.scala
+++ util/nlp_extractor/src/main/scala/Main.scala
@@ -7,7 +7,7 @@ import java.util.Properties
 object Main extends App {
 
   // Parse command line options
-  case class Config(documentKey: String, idKey: String, maxSentenceLength: Int)
+  case class Config(documentKey: String, idKey: String, maxSentenceLength: String)
 
   val parser = new scopt.OptionParser[Config]("DeepDive DocumentParser") {
     head("documentParser", "0.1")
@@ -17,28 +17,33 @@ object Main extends App {
     opt[String]('k', "idKey") required() action { (x, c) =>
       c.copy(idKey = x) 
     } text("JSON key that contains the document id, for example \"documents.id\"")
-    opt[Int]('l', "maxLength") action { (x, c) =>
+    opt[String]('l', "maxLength") action { (x, c) =>
       c.copy(maxSentenceLength = x) 
     } text("Maximum length of sentences to parse (makes things faster) (default: 40)")
   }
 
-  val conf = parser.parse(args, Config("documents.text", "documents.id", 40)) getOrElse { 
+  val conf = parser.parse(args, Config("documents.text", "documents.id", "40")) getOrElse { 
     throw new IllegalArgumentException
   }
+
+  System.err.println(s"""Parsing with id_key="${conf.idKey}" value_key="${conf.documentKey}" """ +
+    s"max_len=${conf.maxSentenceLength}")
 
   // Configuration has been parsed, execute the Document parser
   val props = new Properties()
   props.put("annotators", "tokenize, cleanxml, ssplit, pos, lemma, ner, parse, dcoref")
-  props.put("parse.maxlen", conf.maxSentenceLength.asInstanceOf[java.lang.Object])
-  props.put("pos.maxlen", conf.maxSentenceLength.asInstanceOf[java.lang.Object])
+  props.put("parse.maxlen", conf.maxSentenceLength)
   val dp = new DocumentParser(props)
 
   // Read each json object from stdin and parse the document
-  Source.stdin.getLines.foreach { line =>
+  Source.stdin.getLines.zipWithIndex.foreach { case(line, idx) =>
+
     val jsObj = Json.parse(line).asInstanceOf[JsObject]
     
     val documentId = jsObj.value.get(conf.idKey)
     val documentStr = jsObj.value.get(conf.documentKey).map(_.asInstanceOf[JsString].value)
+
+    System.err.println(s"Parsing document ${documentId.getOrElse("")}...")
 
     // Output a JSON tuple for each sentence
     documentStr.map(dp.parseDocumentString).map(_.sentences).getOrElse(Nil).foreach { sentenceResult =>
