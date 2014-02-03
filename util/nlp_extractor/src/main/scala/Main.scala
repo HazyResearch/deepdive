6df5167731b167f4861eb60923592fd248e0f6ae
Denny Britz <dennybritz@gmail.com>
2014-02-02 17:16:43 -0800

    Multithreaded NLP parsing.

--- util/nlp_extractor/src/main/scala/Main.scala
+++ util/nlp_extractor/src/main/scala/Main.scala
@@ -7,7 +7,7 @@ import java.util.Properties
 object Main extends App {
 
   // Parse command line options
-  case class Config(documentKey: String, idKey: String, maxSentenceLength: String)
+  case class Config(documentKey: String, idKey: String, maxSentenceLength: String, numThreads: String)
 
   val parser = new scopt.OptionParser[Config]("DeepDive DocumentParser") {
     head("documentParser", "0.1")
@@ -20,19 +20,24 @@ object Main extends App {
     opt[String]('l', "maxLength") action { (x, c) =>
       c.copy(maxSentenceLength = x) 
     } text("Maximum length of sentences to parse (makes things faster) (default: 40)")
+    opt[String]('t', "numThreads") action { (x, c) =>
+      c.copy(numThreads = x) 
+    } text("Number of threads (default: # of available cores)")
   }
 
-  val conf = parser.parse(args, Config("documents.text", "documents.id", "40")) getOrElse { 
+  val conf = parser.parse(args, Config("documents.text", "documents.id", "40", 
+      Runtime.getRuntime.availableProcessors.toString)) getOrElse { 
     throw new IllegalArgumentException
   }
 
   System.err.println(s"""Parsing with id_key="${conf.idKey}" value_key="${conf.documentKey}" """ +
-    s"max_len=${conf.maxSentenceLength}")
+    s"max_len=${conf.maxSentenceLength} numThreads=${conf.numThreads}")
 
   // Configuration has been parsed, execute the Document parser
   val props = new Properties()
   props.put("annotators", "tokenize, cleanxml, ssplit, pos, lemma, ner, parse, dcoref")
   props.put("parse.maxlen", conf.maxSentenceLength)
+  props.put("threads", conf.numThreads)
   val dp = new DocumentParser(props)
 
   // Read each json object from stdin and parse the document
