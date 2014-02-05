a166701053e417dda875c146d3ec39a86a286267
Denny Britz <dennybritz@gmail.com>
2014-02-05 11:51:53 -0800

    add redirect to stanford page

--- _includes/js/site.js
+++ _includes/js/site.js
@@ -1,3 +1,7 @@
+if (window.location.href.indexOf('http://dennybritz.github.io') === 0) {
+  window.location.href = 'http://deepdive.stanford.edu/{{ page.url }}';
+}
+
 $(function(){
   analytics.trackLink($("a[href='https://github.com/dennybritz/deepdive/archive/master.zip']"), "click_github_download");
 })
