67e766f6f8e03c574734cd0ae33a03cf751a95af
Denny Britz <dennybritz@gmail.com>
2014-02-04 22:51:31 -0800

    Example update

--- examples/spouse_example/udf/ext_has_spouse_features.py
+++ examples/spouse_example/udf/ext_has_spouse_features.py
@@ -32,17 +32,11 @@ for row in fileinput.input():
   # Feature 2: Number of words between the two phrases
   features.add("num_words_between=%s" % len(words_between))
 
-  # Feature 4: POS tags of the two words
-  left_pos_tags = "-".join(obj["sentences.pos_tags"][p1_start:p1_end])
-  right_pos_tags = "-".join(obj["sentences.pos_tags"][p2_start:p2_end])
-  features.add("pos_tags=(%s,%s)" %(left_pos_tags, right_pos_tags))
-
-  # Feature 5: Does the last word (last name) match assuming the words are not equal?
+  # Feature 3: Does the last word (last name) match assuming the words are not equal?
   last_word_left = obj["sentences.words"][p1_end-1]
   last_word_right = obj["sentences.words"][p2_end-1]
   if (last_word_left == last_word_right) and (p1_text != p2_text):
     features.add("last_word_matches")
-
 
   # TODO: Add more features, look at dependency paths, etc
 
