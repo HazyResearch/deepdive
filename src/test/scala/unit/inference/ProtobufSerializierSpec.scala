fa2698a308f2e0b2fb8ea6212afe3460e33db0ba
Denny Britz <dennybritz@gmail.com>
2014-02-11 11:13:40 -0800

    Improve spec for factor graph serialization.

--- src/test/scala/unit/inference/ProtobufSerializierSpec.scala
+++ src/test/scala/unit/inference/ProtobufSerializierSpec.scala
@@ -27,10 +27,43 @@ class ProtobufSerializerSpec extends FunSpec with BeforeAndAfter {
       // Read the factor graph
       val graph = FactorGraphProtos.FactorGraph.newBuilder
       graph.mergeFrom(new FileInputStream(outputFile))
-      assert(graph.getWeightCount === 2)
-      assert(graph.getVariableCount === 2)
-      assert(graph.getFactorCount === 2)
-      assert(graph.getEdgeCount === 3)
+
+      assert(graph.getWeight(0).getId === 0)
+      assert(graph.getWeight(0).getIsFixed === false)
+      assert(graph.getWeight(0).getInitialValue === 0.0)
+      assert(graph.getWeight(0).getDescription === "someFeature1")
+      assert(graph.getWeight(1).getId === 1)
+      assert(graph.getWeight(1).getIsFixed === true)
+      assert(graph.getWeight(1).getInitialValue === 10.0)
+      assert(graph.getWeight(1).getDescription === "someFeature2")
+
+      assert(graph.getVariable(0).getId === 0)
+      assert(graph.getVariable(0).hasInitialValue === false)
+      assert(graph.getVariable(0).getDataType === FactorGraphProtos.Variable.VariableDataType.BOOLEAN)
+      assert(graph.getVariable(1).getId === 1)
+      assert(graph.getVariable(1).hasInitialValue === false)
+      assert(graph.getVariable(1).getDataType === FactorGraphProtos.Variable.VariableDataType.BOOLEAN)
+
+      assert(graph.getFactor(0).getId === 0)
+      assert(graph.getFactor(0).getWeightId === 0)
+      assert(graph.getFactor(0).getFactorFunction === FactorGraphProtos.Factor.FactorFunctionType.ISTRUE)
+      assert(graph.getFactor(1).getId === 1)
+      assert(graph.getFactor(1).getWeightId === 1)
+      assert(graph.getFactor(1).getFactorFunction === FactorGraphProtos.Factor.FactorFunctionType.IMPLY)
+
+      assert(graph.getEdge(0).getVariableId === 0)
+      assert(graph.getEdge(0).getFactorId === 0)
+      assert(graph.getEdge(0).getPosition === 0)
+      assert(graph.getEdge(0).getIsPositive === true)
+      assert(graph.getEdge(1).getVariableId === 0)
+      assert(graph.getEdge(1).getFactorId === 1)
+      assert(graph.getEdge(1).getPosition === 0)
+      assert(graph.getEdge(1).getIsPositive === true)
+      assert(graph.getEdge(2).getVariableId === 1)
+      assert(graph.getEdge(2).getFactorId === 1)
+      assert(graph.getEdge(2).getPosition === 1)
+      assert(graph.getEdge(2).getIsPositive === false)
+
     }
   
   }
