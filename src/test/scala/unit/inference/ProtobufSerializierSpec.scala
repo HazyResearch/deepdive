72da10be16c0206b8f89176c3c8bc27d8911e5d2
Denny Britz <dennybritz@gmail.com>
2014-02-11 09:24:34 -0800

    improve protobuf spec

--- src/test/scala/unit/inference/ProtobufSerializierSpec.scala
+++ src/test/scala/unit/inference/ProtobufSerializierSpec.scala
@@ -14,21 +14,23 @@ class ProtobufSerializerSpec extends FunSpec with BeforeAndAfter {
       val outputFile = File.createTempFile("ProtobufSerializerSpec", "pb")
       val serializier = new ProtobufSerializer
       serializier.addWeight(0, false, 0.0, "someFeature1")
-      serializier.addWeight(1, false, 0.0, "someFeature2")
+      serializier.addWeight(1, true, 10.0, "someFeature2")
       serializier.addVariable(0, None, "Boolean")
+      serializier.addVariable(1, None, "Boolean")
       serializier.addFactor(0, 0, "IsTrueFactorFunction")
-      serializier.addFactor(1, 1, "IsTrueFactorFunction")
+      serializier.addFactor(1, 1, "ImplyFactorFunction")
+      serializier.addEdge(0, 0, 0, true)
       serializier.addEdge(0, 1, 0, true)
-      serializier.addEdge(1, 1, 0, true)
+      serializier.addEdge(1, 1, 1, false)
       serializier.write(outputFile)
 
       // Read the factor graph
       val graph = FactorGraphProtos.FactorGraph.newBuilder
       graph.mergeFrom(new FileInputStream(outputFile))
       assert(graph.getWeightCount === 2)
-      assert(graph.getVariableCount === 1)
+      assert(graph.getVariableCount === 2)
       assert(graph.getFactorCount === 2)
-      assert(graph.getEdgeCount === 2)
+      assert(graph.getEdgeCount === 3)
     }
   
   }
