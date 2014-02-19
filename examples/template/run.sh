6e6665d6aa896e9f0b818714e6bbd71f5d0326b0
Denny Britz <dennybritz@gmail.com>
2014-02-19 14:09:45 -0800

    Better example template configuration.

--- examples/template/run.sh
+++ examples/template/run.sh
@@ -1,15 +1,6 @@
 #! /bin/bash
 
-export DEEPDIVE_HOME=`cd $(dirname $0)/../..; pwd`
-export APP_HOME=`pwd`
-
-# Database Configuration
-export DBNAME=
-export PGUSER=${PGUSER:-`whoami`}
-export PGPASSWORD=${PGPASSWORD:-}
-export PGPORT=${PGPORT:-5432}
-export PGHOST=${PGHOST:-localhost}
+. "$(dirname $0)/env.sh"
 
 cd $DEEPDIVE_HOME
-
-SBT_OPTS="-Xmx4g" sbt "run -c $APP_HOME/application.conf"
+sbt "run -c $APP_HOME/application.conf"
