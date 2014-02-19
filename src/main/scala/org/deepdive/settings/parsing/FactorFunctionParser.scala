693806438630ab7800b09bcbf907829f3ee92e9d
Denny Britz <dennybritz@gmail.com>
2014-02-19 14:11:02 -0800

    Allow capitalized factor functions

--- src/main/scala/org/deepdive/settings/parsing/FactorFunctionParser.scala
+++ src/main/scala/org/deepdive/settings/parsing/FactorFunctionParser.scala
@@ -8,28 +8,28 @@ object FactorFunctionParser extends RegexParsers with Logging {
   def arrayDefinition = """\[\]""".r
   // def factorFunctionName = "Imply" | "Or" | "And" | "Equal" | "IsTrue"
 
-  def implyFactorFunction = "Imply" ~> "(" ~> rep1sep(factorVariable, ",") <~ ")" ^^ { varList =>
+  def implyFactorFunction = ("Imply" | "IMPLY") ~> "(" ~> rep1sep(factorVariable, ",") <~ ")" ^^ { varList =>
     ImplyFactorFunction(varList)
   }
 
-  def orFactorFunction = "Or" ~> "(" ~> rep1sep(factorVariable, ",") <~ ")" ^^ { varList =>
+  def orFactorFunction = ("Or" | "OR") ~> "(" ~> rep1sep(factorVariable, ",") <~ ")" ^^ { varList =>
     OrFactorFunction(varList)
   }
 
-  def xorFactorFunction = "Xor" ~> "(" ~> rep1sep(factorVariable, ",") <~ ")" ^^ { varList =>
+  def xorFactorFunction = ("Xor" | "XOR") ~> "(" ~> rep1sep(factorVariable, ",") <~ ")" ^^ { varList =>
     XorFactorFunction(varList)
   }
 
-  def andFactorFunction = "And" ~> "(" ~> rep1sep(factorVariable, ",") <~ ")" ^^ { varList =>
+  def andFactorFunction = ("And" | "AND") ~> "(" ~> rep1sep(factorVariable, ",") <~ ")" ^^ { varList =>
     AndFactorFunction(varList)
   }
 
-  def equalFactorFunction = "Equal" ~> "(" ~> factorVariable ~ ("," ~> factorVariable) <~ ")" ^^ { 
+  def equalFactorFunction = ("Equal" | "EQUAL") ~> "(" ~> factorVariable ~ ("," ~> factorVariable) <~ ")" ^^ { 
     case v1 ~ v2 =>
     EqualFactorFunction(List(v1, v2))
   }
 
-  def isTrueFactorFunction = "IsTrue" ~> "(" ~> factorVariable <~ ")" ^^ { variable =>
+  def isTrueFactorFunction = ("IsTrue" | "ISTRUE") ~> "(" ~> factorVariable <~ ")" ^^ { variable =>
     IsTrueFactorFunction(List(variable))
   }
 
