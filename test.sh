a10059844dac66ad986204da2abefc6114297e2f
Denny Britz <dennybritz@gmail.com>
2014-03-05 17:16:32 -0800

    Remove server start from test.sh

--- test.sh
+++ test.sh
@@ -10,11 +10,5 @@ export PGHOST=${PGHOST:-localhost}
 dropdb deepdive_test
 createdb deepdive_test
 
-# Start in-memory SQL server
-java -cp lib/hsqldb.jar org.hsqldb.server.Server --database.0 mem:deepdive_test --dbname.0 deepdive_test &
-
 # Run the test
 SBT_OPTS="-Xmx2g" sbt "test"
-
-# Kill in-memory SQL server
-kill $!
