11b5d16c224f01e1f5d61c972f9cd5e74f99129c
Zifei Shan <zifei@stanford.edu>
2014-03-10 16:24:59 -0700

    Modify spouse to try new sampler

--- examples/spouse_example/run.sh
+++ examples/spouse_example/run.sh
@@ -3,6 +3,8 @@
 export APP_HOME=`pwd`
 export DEEPDIVE_HOME=`cd $(dirname $0)/../..; pwd`
 
+export LD_LIBRARY_PATH="/dfs/rulk/0/hazy_share/lib64/:/dfs/rulk/0/hazy_share/lib/protobuf/lib/:/dfs/rulk/0/hazy_share/lib/tclap/lib/:$LD_LIBRARY_PATH"
+
 # Database Configuration
 export DBNAME=deepdive_spouse
 export PGUSER=${PGUSER:-`whoami`}
