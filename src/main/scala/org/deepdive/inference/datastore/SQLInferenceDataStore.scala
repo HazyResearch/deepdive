e2c3cda02341dd32891682905df5072d62eebb65
Denny Britz <dennybritz@gmail.com>
2014-03-10 15:36:30 -0700

    default count of 0

--- src/main/scala/org/deepdive/inference/datastore/SQLInferenceDataStore.scala
+++ src/main/scala/org/deepdive/inference/datastore/SQLInferenceDataStore.scala
@@ -162,15 +162,14 @@ trait SQLInferenceDataStore extends InferenceDataStore with Logging {
   """
 
   def selectVariablesForDumpSQL = s"""
-    SELECT ${VariablesMapTable}.id AS "id", ${VariablesTable}.is_evidence AS "is_evidence",
-      ${VariablesTable}.data_type AS "data_type",
-      ${VariablesTable}.initial_value AS "initial_value", "edge_count"
-    FROM ${VariablesTable}, ${VariablesMapTable},
+    SELECT ${VariablesMapTable}.id AS "id", is_evidence, data_type, initial_value, edge_count
+    FROM ${VariablesTable} INNER JOIN ${VariablesMapTable}
+      ON ${VariablesTable}.id = ${VariablesMapTable}.variable_id
+    LEFT JOIN
     (SELECT variable_id AS "edges.vid", 
       COUNT(*) as "edge_count" 
-      FROM ${EdgesTable} GROUP BY variable_id) tmp
-    WHERE ${VariablesTable}.id = ${VariablesMapTable}.variable_id
-      AND ${VariablesTable}.id = "edges.vid"
+      FROM ${EdgesTable} GROUP BY variable_id) tmp 
+    ON ${VariablesTable}.id = "edges.vid"
     ORDER BY ${VariablesMapTable}.id ASC;
   """
 
@@ -418,7 +417,7 @@ trait SQLInferenceDataStore extends InferenceDataStore with Logging {
         rs.long("id"),
         if (rs.boolean("is_evidence")) rs.doubleOpt("initial_value") else None,
         rs.string("data_type"), 
-        rs.long("edge_count"),
+        rs.longOpt("edge_count").getOrElse(0),
         None)
     }
     log.info("Serializing factors...")
