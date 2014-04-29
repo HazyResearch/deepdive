0221f7a792d97beadcce9d2bb4452cfc1c0c5f7b
Zifei Shan <zifei@stanford.edu>
2014-04-28 17:16:40 -0700

    Updated OCR examples with better format

--- examples/ocr/prepare_data.sh
+++ examples/ocr/prepare_data.sh
@@ -9,15 +9,44 @@ BASE_DIR=`pwd`
 dropdb $DB_NAME
 createdb $DB_NAME
 
-psql -c "drop schema if exists public cascade; create schema public;" $DB_NAME
+psql -c """
+    DROP SCHEMA IF EXISTS public CASCADE; 
+    CREATE SCHEMA public;
+    """ $DB_NAME
 
-psql -c "create table features(id bigserial primary key, word_id int, feature_id int, feature_val boolean);" $DB_NAME
+psql -c """
+    CREATE TABLE features(
+    id          BIGSERIAL PRIMARY KEY, 
+    word_id     INT, 
+    feature_id  INT, 
+    feature_val BOOLEAN);
+    """ $DB_NAME
 
-psql -c "create table feature_names(fid int primary key, fname varchar(20));" $DB_NAME
-psql -c "create table label1(id bigserial primary key, wid int, val boolean);" $DB_NAME
-psql -c "create table label2(id bigserial primary key, wid int, val boolean);" $DB_NAME
+psql -c """
+    CREATE TABLE feature_names(
+    fid     INT PRIMARY KEY, 
+    fname   VARCHAR(20));
+    """ $DB_NAME
+psql -c """
+    CREATE TABLE label1(
+    id    BIGSERIAL PRIMARY KEY, 
+    wid   INT, val BOOLEAN);
+    """ $DB_NAME
+psql -c """
+    CREATE TABLE label2(
+    id    BIGSERIAL PRIMARY KEY, 
+    wid   INT, val BOOLEAN);
+    """ $DB_NAME
 
-psql -c "COPY features(word_id, feature_id, feature_val) FROM '$BASE_DIR/data/raw/feature_table.csv' DELIMITER ',' CSV;" $DB_NAME
-psql -c "COPY label1(wid, val) FROM '$BASE_DIR/data/raw/label1_table.csv' DELIMITER ',' CSV;" $DB_NAME
-psql -c "alter sequence label2_id_seq restart with 621;" $DB_NAME
-psql -c "COPY label2(wid, val) FROM '$BASE_DIR/data/raw/label2_table.csv' DELIMITER ',' CSV;" $DB_NAME
+psql -c """
+    COPY features(word_id, feature_id, feature_val) 
+    FROM STDIN DELIMITER ',' CSV;
+    """ $DB_NAME <$BASE_DIR/data/raw/feature_table.csv
+psql -c """
+    COPY label1(wid, val) 
+    FROM STDIN DELIMITER ',' CSV;
+    """ $DB_NAME <$BASE_DIR/data/raw/label1_table.csv
+psql -c """
+    COPY label2(wid, val) 
+    FROM STDIN DELIMITER ',' CSV;
+    """ $DB_NAME <$BASE_DIR/data/raw/label2_table.csv
