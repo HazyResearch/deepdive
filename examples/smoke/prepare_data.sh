b9729ec469b3c55c18eef3fbefdce3182f4ef650
Mikhail Sushkov <sushkovm@gmail.com>
2014-05-05 02:35:42 -0700

    changing smoke example; need to get the training correct

--- examples/smoke/prepare_data.sh
+++ examples/smoke/prepare_data.sh
@@ -9,52 +9,67 @@ createdb -p $PGPORT -h $PGHOST $DBNAME
 psql -p $PGPORT -h $PGHOST $DBNAME -c "DROP SCHEMA IF EXISTS public CASCADE; CREATE SCHEMA public;"
 
 psql -p $PGPORT -h $PGHOST $DBNAME -c """
-	DROP TABLE IF EXISTS people1 CASCADE;
-	DROP TABLE IF EXISTS people2 CASCADE;
+	DROP TABLE IF EXISTS person CASCADE;
+	DROP TABLE IF EXISTS person_has_cancer CASCADE;
+	DROP TABLE IF EXISTS person_smokes CASCADE;
 	DROP TABLE IF EXISTS friends CASCADE;
 
-	CREATE TABLE people1 (
-		id bigserial primary key,
+	CREATE TABLE person (
 		person_id bigint,
-		name text,
-		has_cancer boolean
+		name text
 	)
 
-	CREATE TABLE people2 (
-		id bigserial primary key,
+	CREATE TABLE person_has_cancer (
 		person_id bigint,
-		name text,
-		smokes boolean
+		has_cancer boolean,
+		id bigint
+	)
+
+	CREATE TABLE person_smokes (
+		person_id bigint,
+		smokes boolean,
+		id bigint
 	)
 
 	CREATE TABLE friends (
-		id bigserial primary key,
 		person_id bigint,
 		friend_id bigint
 	)
 ;"""
 
 psql -p $PGPORT -h $PGHOST $DBNAME -c """
-	INSERT INTO people1(id, person_id, name) VALUES
-		(1, 1, 'Anna'),
-		(2, 2, 'Bob'),
-		(3, 3, 'Edward'),
-		(4, 4, 'Frank'),
-		(5, 5, 'Gary'),
-		(6, 6, 'Helen')
+	INSERT INTO person(person_id, name) VALUES
+		(1, 'Anna'),
+		(2, 'Bob'),
+		(3, 'Edward'),
+		(4, 'Frank'),
+		(5, 'Gary'),
+		(6, 'Helen')
 ;"""
 
 psql -p $PGPORT -h $PGHOST $DBNAME -c """
-	INSERT INTO people2(id, person_id, name, smokes) VALUES
-		(7, 1, 'Anna', TRUE),
-		(8, 2, 'Bob', NULL),
-		(9, 3, 'Edward', TRUE),
-		(10, 4, 'Frank', NULL),
-		(11, 5, 'Gary', NULL),
-		(12, 6, 'Helen', NULL)
+	INSERT INTO person_smokes(person_id, smokes) VALUES
+		(1, TRUE),
+		(2, FALSE),
+		(3, TRUE),
+		(4, TRUE),
+		(5, TRUE),
+		(6, NULL)
 ;"""
+
+psql -p $PGPORT -h $PGHOST $DBNAME -c """
+	INSERT INTO person_has_cancer(person_id, has_cancer) VALUES
+		(1, TRUE),
+		(2, FALSE),
+		(3, NULL),
+		(4, NULL),
+		(5, NULL),
+		(6, FALSE)
+"""
 
 psql -p $PGPORT -h $PGHOST $DBNAME -c """
 	INSERT INTO friends(person_id, friend_id) VALUES 
-		(1, 2), (1, 3), (1, 4), (3, 4), (5, 6), (2, 1), (3, 1), (4, 1), (4, 3), (6, 5)
+	    (1, 3), (3, 1),
+		(3, 4), (4, 3),
+		(5, 6), (6, 5)
 ;"""
