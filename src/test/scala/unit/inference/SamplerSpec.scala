5e39c0242e1b68b3b4ffa7c5ebc0f117c7bd94de
Zifei Shan <zifei@stanford.edu>
2014-05-06 18:23:16 -0700

    Fixed tests to use new sampler

--- src/test/scala/unit/inference/SamplerSpec.scala
+++ src/test/scala/unit/inference/SamplerSpec.scala
@@ -28,7 +28,7 @@ class SamplerSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSe
     
     it("should work with a trivial factor graph") {
       val sampler = TestActorRef[Sampler]
-      val samplerCmd = "java -Xmx4g -jar util/sampler-assembly-0.1.jar"
+      // val samplerCmd = "java -Xmx4g -jar util/sampler-assembly-0.1.jar"
       val samplerOptions = "-l 10 -s 10 -i 10"
       // val variablesOutputFile = File.createTempFile("sampler_output", "")
       sampler ! Sampler.Run(samplerCmd, samplerOptions, weightsFile, variablesFile, factorsFile, edgesFile,
@@ -39,7 +39,7 @@ class SamplerSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSe
     it("should throw an exception when sampling fails") {
       val sampler = TestActorRef[Sampler]
       watch(sampler)
-      val samplerCmd = "java -Xmx4g -jar util/sampler-assembly-0.1.jar"
+      // val samplerCmd = "java -Xmx4g -jar util/sampler-assembly-0.1.jar"
       val samplerOptions = "-l 10 -s 10 -i 10"
       val variablesOutputFile = File.createTempFile("sampler_output", "")
       intercept[RuntimeException] {
--- test.sh
+++ test.sh
@@ -5,29 +5,37 @@ export PGUSER=${PGUSER:-`whoami`}
 export PGPASSWORD=${PGPASSWORD:-}
 export PGPORT=${PGPORT:-5432}
 export PGHOST=${PGHOST:-localhost}
-
+export DBNAME=deepdive_test
 # Create test database
-dropdb deepdive_test
-createdb deepdive_test
+dropdb $DBNAME
+createdb $DBNAME
+
+export DEEPDIVE_HOME=`cd $(dirname $0); pwd`
+
+cd $DEEPDIVE_HOME/lib
+
+# Add dependencies for Travis system
 
 # Detect OS
 if [ "$(uname)" == "Darwin" ]; then
   # if haven't unzipped dw_mac.zip
-  if [ ! -d $ROOT_PATH/lib/dw_mac ]; then
+  if [ ! -d $DEEPDIVE_HOME/lib/dw_mac ]; then
       unzip dw_mac.zip
     fi
 
-    export LD_LIBRARY_PATH=$ROOT_PATH/lib/dw_mac/lib/protobuf/lib:$ROOT_PATH/lib/dw_mac/lib:$LD_LIBRARY_PATH
-  export DYLD_LIBRARY_PATH=$ROOT_PATH/lib/dw_mac:$DYLD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$DEEPDIVE_HOME/lib/dw_mac/lib/protobuf/lib:$DEEPDIVE_HOME/lib/dw_mac/lib:$LD_LIBRARY_PATH
+  export DYLD_LIBRARY_PATH=$DEEPDIVE_HOME/lib/dw_mac:$DYLD_LIBRARY_PATH
 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # if haven't unzipped dw_linux.zip
-  if [ ! -d $ROOT_PATH/lib/dw_linux ]; then
+  if [ ! -d $DEEPDIVE_HOME/lib/dw_linux ]; then
       unzip dw_linux.zip
     fi
 
-    export LD_LIBRARY_PATH=$ROOT_PATH/lib/dw_linux/lib:$ROOT_PATH/lib/dw_linux/lib64:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$DEEPDIVE_HOME/lib/dw_linux/lib:$DEEPDIVE_HOME/lib/dw_linux/lib64:$LD_LIBRARY_PATH
 fi
+
+cd $DEEPDIVE_HOME
 
 # Run the test
 SBT_OPTS="-Xmx2g" sbt "test"
