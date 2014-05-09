28b29ecfe69ec5da45c801012775c22091a9f598
Zifei Shan <zifei@stanford.edu>
2014-05-09 14:30:43 -0700

    Removed from test.sh unzipping

--- test.sh
+++ test.sh
@@ -12,30 +12,20 @@ export DEEPDIVE_HOME=`cd $(dirname $0); pwd`
 
 cd $DEEPDIVE_HOME/lib
 
-# Add dependencies for Travis system
-
-# Detect OS
-if [ "$(uname)" == "Darwin" ]; then
-  # if haven't unzipped dw_mac.zip
-  if [ ! -d $DEEPDIVE_HOME/lib/dw_mac ]; then
-      unzip dw_mac.zip
-    fi
-
+case $(uname) in
+  Darwin)
     export LD_LIBRARY_PATH=$DEEPDIVE_HOME/lib/dw_mac/lib/protobuf/lib:$DEEPDIVE_HOME/lib/dw_mac/lib:$LD_LIBRARY_PATH
-  export DYLD_LIBRARY_PATH=$DEEPDIVE_HOME/lib/dw_mac:$DYLD_LIBRARY_PATH
+    export DYLD_LIBRARY_PATH=$DEEPDIVE_HOME/lib/dw_mac:$DYLD_LIBRARY_PATH
+    ;;
 
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # if haven't unzipped dw_linux.zip
-  if [ ! -d $DEEPDIVE_HOME/lib/dw_linux ]; then
-      unzip dw_linux.zip
-    fi
-
+  Linux*)
     export LD_LIBRARY_PATH=$DEEPDIVE_HOME/lib/dw_linux/lib:$DEEPDIVE_HOME/lib/dw_linux/lib64:$LD_LIBRARY_PATH
-fi
+    ;;
 
-echo $LD_LIBRARY_PATH
-
-
+  *)
+    echo >&2 "$(uname): Unsupported OS"
+    false
+esac
 
 cd $DEEPDIVE_HOME
 
--- util/fill_sequence.sh
+++ util/fill_sequence.sh
@@ -4,4 +4,5 @@ psql -c """
         DROP SEQUENCE IF EXISTS tmp_sequence_$1;
         CREATE SEQUENCE tmp_sequence_$1;
         UPDATE $1 SET $2 = nextval('tmp_sequence_$1');
+        DROP SEQUENCE tmp_sequence_$1;
 """ $DBNAME
