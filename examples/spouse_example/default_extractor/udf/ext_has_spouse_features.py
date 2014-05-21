203cc73bd174c1a1184584dff5750391e4c35bff
Zifei Shan <zifei@stanford.edu>
2014-05-20 17:00:45 -0700

    Updated spouse default extractor to use bag of words

--- examples/spouse_example/default_extractor/udf/ext_has_spouse_features.py
+++ examples/spouse_example/default_extractor/udf/ext_has_spouse_features.py
@@ -21,19 +21,21 @@ for row in sys.stdin:
   # Features for this pair come in here
   features = set()
   
-  # Feature 1: Words between the two phrases
+  # Feature 1: Bag of words between the two phrases
   left_idx = min(p1_end, p2_end)
   right_idx = max(p1_start, p2_start)
   words_between = obj["words"][left_idx:right_idx]
   if words_between: 
-    features.add("words_between=" + "-".join(words_between))
+    # features.add("words_between=" + "-".join(words_between))
+    for word in words_between:
+      features.add("word_between=" + word)
 
   # Feature 2: Number of words between the two phrases
   features.add("num_words_between=%s" % len(words_between))
 
   # Feature 3: Does the last word (last name) match assuming the words are not equal?
-  last_word_left = obj["words"][p1_end-1]
-  last_word_right = obj["words"][p2_end-1]
+  last_word_left = obj["words"][p1_end - 1]
+  last_word_right = obj["words"][p2_end - 1]
   if (last_word_left == last_word_right) and (p1_text != p2_text):
     features.add("last_word_matches")
 
