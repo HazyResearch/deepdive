3dc300518b98d1dbc43edc1358228a437b784321
Zifei Shan <zifei@stanford.edu>
2014-05-20 17:06:53 -0700

    Extractor updates

--- examples/spouse_example/default_extractor/run.sh
+++ examples/spouse_example/default_extractor/run.sh
@@ -7,7 +7,7 @@ export APP_HOME=`cd $(dirname $0)/; pwd`
 export DEEPDIVE_HOME=`cd $(dirname $0)/../../../; pwd`
 
 # Initialize database
-bash $APP_HOME/../setup_database.sh $DBNAME
+# bash $APP_HOME/../setup_database.sh $DBNAME
 
 cd $DEEPDIVE_HOME
 
