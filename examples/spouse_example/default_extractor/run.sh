5240eebebce7583fbe1fc910d80cd618699f76f4
Zifei Shan <zifei@stanford.edu>
2014-05-26 14:28:02 -0700

    Small fixes of spouse_example run script

--- examples/spouse_example/default_extractor/run.sh
+++ examples/spouse_example/default_extractor/run.sh
@@ -7,11 +7,11 @@ export APP_HOME=`cd $(dirname $0)/; pwd`
 export DEEPDIVE_HOME=`cd $(dirname $0)/../../../; pwd`
 
 # Initialize database
-# bash $APP_HOME/../setup_database.sh $DBNAME
+bash $APP_HOME/../setup_database.sh $DBNAME
 
 # Using ddlib
 export PYTHONPATH=$DEEPDIVE_HOME/ddlib:$PYTHONPATH
 
 cd $DEEPDIVE_HOME
 
-SBT_OPTS="-Xmx4g" sbt "run -c $APP_HOME/application.conf"
+sbt "run -c $APP_HOME/application.conf"
