8569ecc01fc112379b2cf83d8127a21be31201db
Zifei Shan <zifei@stanford.edu>
2014-08-20 22:11:59 -0700

    Integrating linkchecker to Travis (debugging)

---
+++ src/test/bash/linkcheck.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+export DEEPDIVE_HOME=`cd $(dirname $0)/../../../; pwd`
+
+# Run linkchecker and send output to the log file
+linkchecker http://deepdive.stanford.edu/ >$DEEPDIVE_HOME/linkchecker_errorlog.txt
+
+# Look for string "Error" in the output log
+line=$(grep Error $DEEPDIVE_HOME/linkchecker_errorlog.txt)
+
+if [ $? -eq 0 ]; then
+  echo "[`date`] Errors found:"
+  cat $DEEPDIVE_HOME/linkchecker_errorlog.txt
+  rm -f $DEEPDIVE_HOME/linkchecker_errorlog.txt
+  exit 1;
+else
+  echo "[`date`] No errors found."
+  rm -f $DEEPDIVE_HOME/linkchecker_errorlog.txt
+fi
