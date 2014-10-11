afeeb9a098c5f482f7a73dacdd3cea8218f030b0
Zifei Shan <zifei@stanford.edu>
2014-10-11 03:43:26 -0400

    Use user.dir instead of '.'; Added docs for DEEPDIVE_HOME

--- src/main/scala/org/deepdive/Context.scala
+++ src/main/scala/org/deepdive/Context.scala
@@ -20,11 +20,10 @@ object Context extends Logging {
   // Set deepdive home according to environment variable. If not specified, 
   // use user's current directory
   val deepdiveHome = System.getenv("DEEPDIVE_HOME") match {
-    case null => "."
+    case null => System.getProperty("user.dir")
     case _ => System.getenv("DEEPDIVE_HOME")
   }
   
-//      System.getProperty("user.dir"))
 
   def shutdown(exitValue: Int = 0) {
     JdbcDataStore.close()
