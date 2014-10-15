67ebd2cd80bf79346cd09160e535c97a71974bd4
Matteo Riondato <rionda@stanford.edu>
2014-10-15 11:49:29 -0700

    Whoops, add two missing ')'

--- examples/spouse_example/tsv_extractor/run.sh
+++ examples/spouse_example/tsv_extractor/run.sh
@@ -6,15 +6,19 @@ export DEEPDIVE_HOME=`cd $(dirname $0)/../../../; pwd`
 # Database Configuration
 export DBNAME=deepdive_spouse_tsv
 
-export PGUSER=${PGUSER:-`whoami`}
-export PGPASSWORD=${PGPASSWORD:-}
-export PGPORT=${PGPORT:-5432}
-export PGHOST=${PGHOST:-localhost}
+#export PGUSER=${PGUSER:-`whoami`}
+#export PGPASSWORD=${PGPASSWORD:-}
+#export PGPORT=${PGPORT:-5432}
+#export PGHOST=${PGHOST:-localhost}
+export PGUSER=senwu
+export PGPORT=6432
+export PGPASSWORD=""
+export PGHOST=raiders1
 
 # Initialize database
 bash $APP_HOME/../setup_database.sh $DBNAME
 
 cd $DEEPDIVE_HOME
 
-# SBT_OPTS="-Xmx4g" sbt "run -c $APP_HOME/application.conf"
-deepdive -c $APP_HOME/application.conf
+ SBT_OPTS="-Xmx4g" sbt "run -c $APP_HOME/application.conf"
+#deepdive -c $APP_HOME/application.conf
