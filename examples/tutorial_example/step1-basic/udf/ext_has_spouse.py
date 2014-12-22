1b8595bca45d72d09ee2495a5ed46e06431b79b7
Zifei Shan <zifei@stanford.edu>
2014-12-22 11:37:57 -0800

    Updated basic code to add DS rule and holdout by sentence

--- examples/tutorial_example/step1-basic/udf/ext_has_spouse.py
+++ examples/tutorial_example/step1-basic/udf/ext_has_spouse.py
@@ -8,13 +8,13 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 # Load the spouse dictionary for distant supervision.
 # A person can have multiple spouses
 spouses = set()
-# married_people = set()
+married_people = set()
 lines = open(BASE_DIR + '/../data/spouses.tsv').readlines()
 for line in lines:
   name1, name2, relation = line.strip().split('\t')
   spouses.add((name1, name2))  # Add a spouse relation pair
-  # married_people.add(name1)    # Record the person as married
-  # married_people.add(name2)
+  married_people.add(name1)    # Record the person as married
+  married_people.add(name2)
 
 # Load relations of people that are not spouse
 # The non-spouse KB lists incompatible relations, e.g. childrens, siblings, parents.
@@ -45,9 +45,9 @@ for row in sys.stdin:
   # DS rule 3: false if they appear to be in same person
   elif (p1_text == p2_text) or (p1_text in p2_text) or (p2_text in p1_text):
     is_true = '0'
-  # # DS rule 4 false if they are both married, but not married to each other:
-  # elif p1_text_lower in married_people and p2_text_lower in married_people:
-  #   is_true = '0'
+  # DS rule 4 false if they are both married, but not married to each other:
+  elif p1_text_lower in married_people and p2_text_lower in married_people:
+    is_true = '0'
 
   # Output relation candidates into output table
   print '\t'.join([
