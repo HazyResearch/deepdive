4d2cb56031fedacf017a63f25d4c5e96bf186197
Zifei Shan <zifei@stanford.edu>
2014-12-22 10:52:44 -0800

    Updated step-1 labeling. Precision: %7

--- examples/tutorial_example/step1-basic/labeling/prepare-data-from-braindump.sh
+++ examples/tutorial_example/step1-basic/labeling/prepare-data-from-braindump.sh
@@ -3,4 +3,5 @@ set -e
 APP_HOME=`cd $(dirname $0)/../; pwd`
 TASK_DIR="$APP_HOME/labeling/spouse_example-precision_with_features"
 cp -f $APP_HOME/experiment-reports/latest/inference/has_spouse.csv $TASK_DIR/input.csv
+rm -f $TASK_DIR/tags.json
 echo "Mindtagger input file copied to $TASK_DIR/input.csv"
