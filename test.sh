7c343020075704ec9b1d829361cd7ff3b86e493b
Matteo Riondato <rionda@stanford.edu>
2015-01-09 21:51:51 -0800

    Don't run twice on greenplum.

--- test.sh
+++ test.sh
@@ -1,20 +1,19 @@
 set -e
 
+# if greenplum supported (gpfdist exist), then test GP
+if hash gpfdist 2>/dev/null; then
+  echo "Testing Greenplum..."
+  bash test/test_gp.sh $1
 # if command psql exist, then test psql
-if hash psql 2>/dev/null; then
+elif hash psql 2>/dev/null; then
   echo "Testing psql..."
-  bash test/test_psql.sh
+  bash test/test_psql.sh $1
 fi
 
 # if command mysql exist, then test mysql
 if hash mysql 2>/dev/null; then
   echo "Testing mysql..."
-  bash test/test_mysql.sh
+  bash test/test_mysql.sh $1
 fi
 
-# if greenplum supported (gpfdist exist), then test GP
-if hash gpfdist 2>/dev/null; then
-  echo "Testing Greenplum..."
-  bash test/test_gp.sh
-fi
 
