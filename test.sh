bc2f68b4e15e36bf192604cd6f979f5598a2a380
Matteo Riondato <rionda@stanford.edu>
2015-01-12 15:19:03 -0500

    Reorganize and add comments.

--- test.sh
+++ test.sh
@@ -1,19 +1,24 @@
 set -e
 
-# if greenplum supported (gpfdist exist), then test GP
+# if the command psql exists, then test PostgreSQL
+if hash psql 2>/dev/null; then
+	echo "Testing psql..."
+	bash test/test_psql.sh
+fi
+
+# if Greenplum is supported (gpfdist exist), then test Greenplum
+# Since greenplum also has a 'psql' command, the condition in the previous test
+# will also be satisfied. Tests are effectively run twice on GP: one using only
+# the PostgreSQL features and one using the GreenPlum features (e.g., parallel
+# grounding, gpfdist)
 if hash gpfdist 2>/dev/null; then
-  echo "Testing Greenplum..."
-  bash test/test_gp.sh $1
-# if command psql exist, then test psql
-elif hash psql 2>/dev/null; then
-  echo "Testing psql..."
-  bash test/test_psql.sh $1
+	echo "Testing Greenplum..."
+	bash test/test_gp.sh
 fi
 
-# if command mysql exist, then test mysql
+# if the command mysql exists, then test mysql
 if hash mysql 2>/dev/null; then
-  echo "Testing mysql..."
-  bash test/test_mysql.sh $1
+	echo "Testing mysql..."
+	bash test/test_mysql.sh
 fi
-
 
