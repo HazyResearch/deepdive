6cbaff443791943cf2ffbadb3a230ae39e238eeb
Matteo Riondato <rionda@stanford.edu>
2015-01-12 14:16:47 -0500

    Add more comments in the body of the functions, and rephrase some before them. Reorder some if conditions to improve the clarify of the flow (we check the condition that may allow us to return early.

--- src/dstruct/factor_graph/factor.hxx
+++ src/dstruct/factor_graph/factor.hxx
@@ -11,12 +11,15 @@ namespace dd{
 	  const VariableIndex & vid, const VariableValue & proposal) const{
 
 	  const VariableInFactor & vif = vifs[n_start_i_vif];
+	  /* We use the value of the first variable in the factor as the "gold" standard" */
 	  const bool firstsat = (vif.vid == vid) ? vif.satisfiedUsing(proposal) :
 	  	  vif.satisfiedUsing(var_values[vif.vid]);
 
+	  /* Iterate over the factor variables */
 	  for(long i_vif=n_start_i_vif; (i_vif<n_start_i_vif+n_variables);i_vif++){
 	    const VariableInFactor & vif = vifs[i_vif];
 	    const bool satisfied = (vif.vid == vid) ? vif.satisfiedUsing(proposal) : vif.satisfiedUsing(var_values[vif.vid]) ;
+	    /* Early return as soon as we find a mismatch */
 	    if(satisfied != firstsat) return 0.0;
 	  }
 	  return 1.0;
@@ -33,9 +36,11 @@ namespace dd{
 	  const VariableIndex & vid, 
 	  const VariableValue & proposal) const{
 
+	  /* Iterate over the factor variables */
 	  for(long i_vif=n_start_i_vif; (i_vif<n_start_i_vif+n_variables);i_vif++){
 	    const VariableInFactor & vif = vifs[i_vif];
 	    const bool satisfied = (vif.vid == vid) ? vif.satisfiedUsing(proposal) : vif.satisfiedUsing(var_values[vif.vid]) ;
+	    /* Early return as soon as we find a variable that is not satisfied */
 	    if(!satisfied) return 0.0;
 	  }
 	  return 1.0;
@@ -53,25 +58,27 @@ namespace dd{
 	  const VariableIndex & vid, 
 	  const VariableValue & proposal) const{
 
+	  /* Iterate over the factor variables */
 	  for(long i_vif=n_start_i_vif; (i_vif<n_start_i_vif+n_variables);i_vif++){
 	    const VariableInFactor & vif = vifs[i_vif];
 	    const bool satisfied = (vif.vid == vid) ? vif.satisfiedUsing(proposal) : vif.satisfiedUsing(var_values[vif.vid]) ;
+	    /* Early return as soon as we find a variable that is satisfied */
 	    if(satisfied) return 1.0;
 	  }
 	  return 0.0;
 
 	}
 
-
 	/** Return the value of the 'imply (MLN version)' of the variables in the
 	 * factor, with the variable of index vid (wrt the factor) is set to the
 	 * value of the 'proposal' argument.
 	 *
 	 * The head of the 'imply' rule is stored as the *last* variable in the
-	 * factor. 
+	 * factor.
 	 *
 	 * The truth table of the 'imply (MLN version)' function requires to return
-	 * 0.0 if the body of the imply is true but the head is false.
+	 * 0.0 if the body of the imply is satisfied but the head is not, and to
+	 * return 1.0 if the body is not satisfied.
 	 *
 	 */
 	inline double dd::CompactFactor::_potential_imply_mln(
@@ -80,20 +87,21 @@ namespace dd{
 	  const VariableIndex & vid, 
 	  const VariableValue & proposal) const{
 
-	  bool bBody = true; // 
-	  for(long i_vif=n_start_i_vif; 
-	    (i_vif<n_start_i_vif+n_variables - 1) // shortcut && bBody
-	      ;i_vif++){
+	  /* Compute the value of the body of the rule */
+	  bool bBody = true;
+	  for(long i_vif=n_start_i_vif; (i_vif<n_start_i_vif+n_variables - 1); i_vif++){
 	    const VariableInFactor & vif = vifs[i_vif];
-	    // if it is the proposal variable, we use the truth value of the proposal
+	    // If it is the proposal variable, we use the truth value of the proposal
 	    bBody &= (vif.vid == vid) ? vif.satisfiedUsing(proposal) : vif.satisfiedUsing(var_values[vif.vid]) ;
 	  }
-	  if(bBody) {
+	  if(!bBody) {
+	  	// Early return if the body is not satisfied
+	    return 1.0;
+	  } else {
+	  	// Compute the value of the head of the rule */
 	    const VariableInFactor & vif = vifs[n_start_i_vif + n_variables - 1]; // encoding of the head, should be more structured.
 	    const bool bHead = (vif.vid == vid) ? vif.satisfiedUsing(proposal) : vif.satisfiedUsing(var_values[vif.vid]) ;
 	    return bHead ? 1.0 : 0.0;
-	  } else {
-	    return 1.0;
 	  }
 
 	}
@@ -105,8 +113,9 @@ namespace dd{
 	 * The head of the 'imply' rule is stored as the *last* variable in the
 	 * factor. 
 	 *
-	 * The truth table of the 'imply (MLN version)' function requires to return
-	 * -1.0 if the body of the imply is true but the head is false.
+	 * The truth table of the 'imply' function requires to return
+	 * -1.0 if the body of the rule is satisfied but the head is not, and to
+	 *  return 0.0 if the body is not satisfied.
 	 *
 	 */
 	inline double dd::CompactFactor::_potential_imply(
@@ -115,20 +124,21 @@ namespace dd{
 	  const VariableIndex & vid, 
 	  const VariableValue & proposal) const{
 
-	  bool bBody = true; // 
-	  for(long i_vif=n_start_i_vif; 
-	    (i_vif<n_start_i_vif+n_variables - 1) // shortcut && bBody
-	      ;i_vif++){
+	  /* Compute the value of the body of the rule */
+	  bool bBody = true;
+	  for(long i_vif=n_start_i_vif; (i_vif<n_start_i_vif+n_variables - 1); i_vif++){
 	    const VariableInFactor & vif = vifs[i_vif];
-	    // if it is the proposal variable, we use the truth value of the proposal
+	    // If it is the proposal variable, we use the truth value of the proposal
 	    bBody &= (vif.vid == vid) ? vif.satisfiedUsing(proposal) : vif.satisfiedUsing(var_values[vif.vid]) ;
 	  }
-	  if(bBody) {
+	  if(!bBody) {
+	  	// Early return if the body is not satisfied
+	    return 0.0;
+	  } else {
+	  	// Compute the value of the head of the rule */
 	    const VariableInFactor & vif = vifs[n_start_i_vif + n_variables - 1]; // encoding of the head, should be more structured.
 	    bool bHead = (vif.vid == vid) ? vif.satisfiedUsing(proposal) : vif.satisfiedUsing(var_values[vif.vid]) ;
 	    return bHead ? 1.0 : -1.0;
-	  } else {
-	    return 0.0;
 	  }
 
 	}
