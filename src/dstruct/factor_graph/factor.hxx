5a964f1f511fdf2c7625f00e2e6a20fc786cbab2
Matteo Riondato <rionda@stanford.edu>
2015-01-12 13:55:23 -0500

    Add comments and remove obsolete (?) comments

--- src/dstruct/factor_graph/factor.hxx
+++ src/dstruct/factor_graph/factor.hxx
@@ -1,5 +1,10 @@
 namespace dd{
 
+	/** Return the value of the "equality test" of the variables in the factor,
+	 * with the variable of index vid (wrt the factor) is set to the value of
+	 * the 'proposal' argument.
+	 *
+	 */
 	inline double dd::CompactFactor::_potential_equal(
 	  const VariableInFactor * const vifs,
 	  const VariableValue * const var_values, 
@@ -17,6 +22,11 @@ namespace dd{
 	  return 1.0;
 	}
 
+	/** Return the value of the logical AND of the variables in the factor, with
+	 * the variable of index vid (wrt the factor) is set to the value of the
+	 * 'proposal' argument.
+	 *
+	 */
 	inline double dd::CompactFactor::_potential_and(
 	  const VariableInFactor * const vifs,
 	  const VariableValue * const var_values, 
@@ -32,7 +42,11 @@ namespace dd{
 
 	}
 
-
+	/** Return the value of the logical OR of the variables in the factor, with 
+	 * the variable of index vid (wrt the factor) is set to the value of the
+	 * 'proposal' argument.
+	 *
+	 */
 	inline double dd::CompactFactor::_potential_or(
 	  const VariableInFactor * const vifs,
 	  const VariableValue * const var_values, 
@@ -49,6 +63,17 @@ namespace dd{
 	}
 
 
+	/** Return the value of the 'imply (MLN version)' of the variables in the
+	 * factor, with the variable of index vid (wrt the factor) is set to the
+	 * value of the 'proposal' argument.
+	 *
+	 * The head of the 'imply' rule is stored as the *last* variable in the
+	 * factor. 
+	 *
+	 * The truth table of the 'imply (MLN version)' function requires to return
+	 * 0.0 if the body of the imply is true but the head is false.
+	 *
+	 */
 	inline double dd::CompactFactor::_potential_imply_mln(
 	  const VariableInFactor * const vifs,
 	  const VariableValue * const var_values, 
@@ -61,8 +86,6 @@ namespace dd{
 	      ;i_vif++){
 	    const VariableInFactor & vif = vifs[i_vif];
 	    // if it is the proposal variable, we use the truth value of the proposal
-	    // This is a crazy abstraction here, why doesn't vif able to say that it is true or false
-	    //bool bTrue = (vif.vid == vif) ? vif.value.equal(proposal) : vif.value.equal(var_values[vif.vid]) ;
 	    bBody &= (vif.vid == vid) ? vif.satisfiedUsing(proposal) : vif.satisfiedUsing(var_values[vif.vid]) ;
 	  }
 	  if(bBody) {
@@ -75,6 +98,17 @@ namespace dd{
 
 	}
 
+	/** Return the value of the 'imply' of the variables in the factor, with the
+	 * variable of index vid (wrt the factor) is set to the value of the
+	 * 'proposal' argument.
+	 *
+	 * The head of the 'imply' rule is stored as the *last* variable in the
+	 * factor. 
+	 *
+	 * The truth table of the 'imply (MLN version)' function requires to return
+	 * -1.0 if the body of the imply is true but the head is false.
+	 *
+	 */
 	inline double dd::CompactFactor::_potential_imply(
 	  const VariableInFactor * const vifs,
 	  const VariableValue * const var_values, 
@@ -87,8 +121,6 @@ namespace dd{
 	      ;i_vif++){
 	    const VariableInFactor & vif = vifs[i_vif];
 	    // if it is the proposal variable, we use the truth value of the proposal
-	    // This is a crazy abstraction here, why doesn't vif able to say that it is true or false
-	    //bool bTrue = (vif.vid == vif) ? vif.value.equal(proposal) : vif.value.equal(var_values[vif.vid]) ;
 	    bBody &= (vif.vid == vid) ? vif.satisfiedUsing(proposal) : vif.satisfiedUsing(var_values[vif.vid]) ;
 	  }
 	  if(bBody) {
