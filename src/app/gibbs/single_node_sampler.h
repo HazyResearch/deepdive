1469151080a90786ded24f26e32d98a1a1f26ec1
feiranwang <feiran.wang1@gmail.com>
2015-01-13 15:42:34 -0800

    add comments to single_node_sampler.*

--- src/app/gibbs/single_node_sampler.h
+++ src/app/gibbs/single_node_sampler.h
@@ -8,31 +8,58 @@
 #define _SINGLE_NODE_SAMPLER_H
 
 namespace dd{
+  // single thread sample task
   void gibbs_single_thread_task(FactorGraph * const _p_fg, int i_worker, int n_worker);
-
+  // single thread sgd task
   void gibbs_single_thread_sgd_task(FactorGraph * const _p_fg, int i_worker, int n_worker);
 
+  /**
+   * Class for a single NUMA node sampler
+   */
   class SingleNodeSampler{
 
   public:
+    // factor graph
     FactorGraph * const p_fg;
-
+    // number of threads
     int nthread;
+    // node id
     int nodeid;
 
+    // worker for sampling (used in inference)
     SingeNodeWorker<FactorGraph, gibbs_single_thread_task> * sample_worker;
+    // worker for learning
     SingeNodeWorker<FactorGraph, gibbs_single_thread_sgd_task> * sgd_worker;
 
+    /**
+     * Constructs a SingleNodeSampler given factor graph, number of threads, and
+     * node id.
+     */
     SingleNodeSampler(FactorGraph * _p_fg, int _nthread, int _nodeid);
 
+    /**
+     * Clears the inference results in this sampler
+     */
     void clear_variabletally();
 
+    /**
+     * Performs sample
+     */
     void sample();
 
+    /**
+     * Waits for sample worker to finish
+     */
     void wait();
 
+    /**
+     * Performs SGD
+     */
     void sample_sgd();
 
+    /**
+     * Waits for sgd worker to finish
+     */
     void wait_sgd();
 
   };
