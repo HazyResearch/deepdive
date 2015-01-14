2a7a7d7b89da4d19843310724c6c79bc38542af5
feiranwang <feiran.wang1@gmail.com>
2015-01-13 16:07:43 -0800

    add comments to single_thread_sampler

--- src/app/gibbs/single_thread_sampler.cpp
+++ src/app/gibbs/single_thread_sampler.cpp
@@ -12,9 +12,12 @@ namespace dd{
 
 	void SingleThreadSampler::sample(const int & i_sharding, const int & n_sharding){
 	  long nvar = p_fg->n_var;
+	  // calculates the start and end id in this partition
 	  long start = ((long)(nvar/n_sharding)+1) * i_sharding;
 	  long end = ((long)(nvar/n_sharding)+1) * (i_sharding+1);
 	  end = end > nvar ? nvar : end;
+
+	  // sample each variable in the partition
 	  for(long i=start; i<end; i++){
 	    this->sample_single_variable(i);
 	  }
@@ -31,19 +34,33 @@ namespace dd{
 	}
 
 	void SingleThreadSampler::sample_sgd_single_variable(long vid){
+
+		// stochastic gradient ascent 
+    // gradient of weight = E[f|D] - E[f], where D is evidence variables, 
+    // f is the factor function, E[] is expectation. Expectation is calculated
+    // using a sample of the variable.
 	  
 	  Variable & variable = p_fg->variables[vid];
 
+	  // only sample evidence variables
+	  // TODO is this correct?
 	  if(variable.is_evid == false){
 	      return;
 	  }
 
-	  if(variable.domain_type == DTYPE_BOOLEAN){
+	  if(variable.domain_type == DTYPE_BOOLEAN){ // boolean
 
+	      // sample the variable with evidence unchanged
 	      if(variable.is_evid == false){
+	      	// calculate the potential if the variable is positive or negative
 	        potential_pos = p_fg->template potential<false>(variable, 1);
 	        potential_neg = p_fg->template potential<false>(variable, 0);
 	        *this->p_rand_obj_buf = erand48(this->p_rand_seed);
+
+	        // sample the variable
+	        // flip a coin with probability 
+	        // (exp(potential_pos) + exp(potential_neg)) / exp(potential_neg)
+	        // = exp(potential_pos - potential_neg) + 1
 	        if((*this->p_rand_obj_buf) * (1.0 + exp(potential_neg-potential_pos)) < 1.0){
 	          p_fg->template update<false>(variable, 1.0);
 	        }else{
@@ -51,6 +68,7 @@ namespace dd{
 	        }
 	      }
 
+	      // sample the variable regardless of whether it's evidence
 	      potential_pos_freeevid = p_fg->template potential<true>(variable, 1);
 	      potential_neg_freeevid = p_fg->template potential<true>(variable, 0);
 
@@ -63,8 +81,9 @@ namespace dd{
 
 	      this->p_fg->update_weight(variable);
 	      
-	  }else if(variable.domain_type == DTYPE_MULTINOMIAL){
+	  }else if(variable.domain_type == DTYPE_MULTINOMIAL){ // multinomial
 
+	  	// varlen_potential_buffer contains potential for each proposals
 	    while(variable.upper_bound >= varlen_potential_buffer.size()){
 	      varlen_potential_buffer.push_back(0.0);
 	    }
@@ -73,11 +92,14 @@ namespace dd{
 	      sum = -100000.0;
 	      acc = 0.0;
 	      multi_proposal = -1;
+	      
+	      // calculate potential for each proposal
 	      for(int propose=0;propose <= variable.upper_bound; propose++){
 	        varlen_potential_buffer[propose] = p_fg->template potential<false>(variable, propose);
 	        sum = logadd(sum, varlen_potential_buffer[propose]);
 	      }
 
+	      // flip a coin
 	      *this->p_rand_obj_buf = erand48(this->p_rand_seed);
 	      for(int propose=0;propose <= variable.upper_bound; propose++){
 	        acc += exp(varlen_potential_buffer[propose]-sum);
@@ -115,11 +137,13 @@ namespace dd{
 	    //std::cout << "[ERROR] Only Boolean and Multinomial variables are supported now!" << std::endl;
 	    //assert(false);
 	    return;
-	  }
+	  } // end if for variable types
 	  
 	}
 
 	void SingleThreadSampler::sample_single_variable(long vid){
+
+		// this function uses the same sampling technique as in sample_sgd_single_variable
 
 	  Variable & variable = this->p_fg->variables[vid];
 
