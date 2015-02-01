14fac64501df88493b00cc764afc3ea518dfb15d
feiranwang <feiran.wang1@gmail.com>
2015-01-31 22:40:00 -0800

    add enum for variables and factor functions

--- src/main/scala/org/deepdive/inference/datastore/InferenceTableNameSpace.scala
+++ src/main/scala/org/deepdive/inference/datastore/InferenceTableNameSpace.scala
@@ -1,5 +1,5 @@
 package org.deepdive.inference
-
+import org.deepdive.settings._
 
 object InferenceTableNameSpace {
   val weightTablePrefix = "dd_weights_"
@@ -10,5 +10,24 @@ object InferenceTableNameSpace {
   def getQueryTableName(tableName: String) = s"${queryTablePrefix}${tableName}"
   def getFactorTableName(tableName: String) = s"${factorTablePrefix}${tableName}"
 
+  // variable data type id
+  def getVariableDataTypeId(variable: VariableDataType) : Int = {
+    variable match {
+      case BooleanType => 0
+      case MultinomialType(x) => 1
+    }
+  }
+
+  // factor funce type id
+  def getFactorFunctionTypeid(functionName: String) : Int = {
+    functionName match {
+      case "ImplyFactorFunction" => 0
+      case "OrFactorFunction" => 1
+      case "AndFactorFunction" => 2
+      case "EqualFactorFunction" => 3
+      case "IsTrueFactorFunction" =>  4
+      case "MultinomialFactorFunction" => 5
+    }
+  }
 }
 
