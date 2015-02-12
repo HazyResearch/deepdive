c3fd070f901b4b729726bcb7d0d5bc547a0d4781
feiranwang <feiran.wang1@gmail.com>
2015-02-11 20:00:51 -0800

    fix indentation

--- src/main/scala/org/deepdive/datastore/PostgresDataStore.scala
+++ src/main/scala/org/deepdive/datastore/PostgresDataStore.scala
@@ -38,77 +38,77 @@ class PostgresDataStore extends JdbcDataStore with Logging {
       dataReader.close()
     }
 
-      /**
-     * input: iterator (of what?)  
-     * 
-     * - Create a temp CSV file
-     * - run writeCopyData to write   
-     */
-    override def addBatch(result: Iterator[JsObject], outputRelation: String) : Unit = {
-      val file = File.createTempFile(s"deepdive_$outputRelation", ".csv")
-      log.debug(s"Writing data of to file=${file.getCanonicalPath}")
-      val writer = new PrintWriter(new BufferedWriter(new FileWriter(file, true)))
-      // Write the dataset to the file for the relation
-      writeCopyData(result, writer)
-      writer.close()
-      val columnNames = scalikejdbc.DB.getColumnNames(outputRelation).toSet
-      val copySQL = buildCopySql(outputRelation, columnNames)
-      log.debug(s"Copying batch data to postgres. sql='${copySQL}'" +
-        s"file='${file.getCanonicalPath}'")
-      withConnection { implicit connection =>
-        Try(copyBatchData(copySQL, file)) match {
-          case Success(_) => 
-            log.debug("Successfully copied batch data to postgres.") 
-            file.delete()
-          case Failure(ex) => 
-            log.error(s"Error during copy: ${ex}")
-            log.error(s"Problematic CSV file can be found at file=${file.getCanonicalPath}")
-            throw ex
-        }
-      } 
-    }
-
-    /* Builds a COPY statement for a given relation and column names */
-    def buildCopySql(relationName: String, keys: Set[String]) = {
-      // Zifei: do not fill ID any more
-      // val fields = List("id") ++ keys.filterNot(_ == "id").toList.sorted
-      val fields = keys.filterNot(_ == "id").toList.sorted
-      s"""COPY ${relationName}(${fields.mkString(", ")}) FROM STDIN CSV"""
-    }
-
-    /* Builds a CSV dat astring for given JSON data and column names */
-    def writeCopyData(data: Iterator[JsObject], fileWriter: Writer) : Unit = {
-      val writer = new CSVWriter(fileWriter)
-      for (obj <- data) { 
-        val dataList = obj.value.filterKeys(_ != "id").toList.sortBy(_._1)
-        val strList = dataList.map (x => jsValueToString(x._2))
-        // // We get a unique id for the record
-        // val id = variableIdCounter.getAndIncrement().toString
-        // writer.writeNext((Seq(id) ++ strList)toArray)
-        writer.writeNext((strList)toArray)
+  /**
+   * input: iterator (of what?)  
+   * 
+   * - Create a temp CSV file
+   * - run writeCopyData to write   
+   */
+  override def addBatch(result: Iterator[JsObject], outputRelation: String) : Unit = {
+    val file = File.createTempFile(s"deepdive_$outputRelation", ".csv")
+    log.debug(s"Writing data of to file=${file.getCanonicalPath}")
+    val writer = new PrintWriter(new BufferedWriter(new FileWriter(file, true)))
+    // Write the dataset to the file for the relation
+    writeCopyData(result, writer)
+    writer.close()
+    val columnNames = scalikejdbc.DB.getColumnNames(outputRelation).toSet
+    val copySQL = buildCopySql(outputRelation, columnNames)
+    log.debug(s"Copying batch data to postgres. sql='${copySQL}'" +
+      s"file='${file.getCanonicalPath}'")
+    withConnection { implicit connection =>
+      Try(copyBatchData(copySQL, file)) match {
+        case Success(_) => 
+          log.debug("Successfully copied batch data to postgres.") 
+          file.delete()
+        case Failure(ex) => 
+          log.error(s"Error during copy: ${ex}")
+          log.error(s"Problematic CSV file can be found at file=${file.getCanonicalPath}")
+          throw ex
       }
-    }
+    } 
+  }
 
-    /* Translates a JSON value to a String that can be insert using COPY statement */
-    private def jsValueToString(x: JsValue) : String = x match {
-      case JsString(x) => x.replace("\\", "\\\\")
-      case JsNumber(x) => x.toString
-      case JsNull => null
-      case JsBoolean(x) => x.toString
-      case JsArray(x) => 
-        val innerData = x.map {
-          case JsString(x) => 
-            val convertedStr = jsValueToString(JsString(x))
-            val escapedStr = convertedStr.replace("\"", "\\\"")
-            s""" "${escapedStr}" """ 
-          case x: JsValue => jsValueToString(x)
-        }.mkString(",")
-        val arrayStr = s"{${innerData}}"
-        arrayStr
-      case x : JsObject => Json.stringify(x)
-      case _ =>
-        log.warning(s"Could not convert JSON value ${x} to String")
-        ""
+  /* Builds a COPY statement for a given relation and column names */
+  def buildCopySql(relationName: String, keys: Set[String]) = {
+    // Zifei: do not fill ID any more
+    // val fields = List("id") ++ keys.filterNot(_ == "id").toList.sorted
+    val fields = keys.filterNot(_ == "id").toList.sorted
+    s"""COPY ${relationName}(${fields.mkString(", ")}) FROM STDIN CSV"""
+  }
+
+  /* Builds a CSV dat astring for given JSON data and column names */
+  def writeCopyData(data: Iterator[JsObject], fileWriter: Writer) : Unit = {
+    val writer = new CSVWriter(fileWriter)
+    for (obj <- data) { 
+      val dataList = obj.value.filterKeys(_ != "id").toList.sortBy(_._1)
+      val strList = dataList.map (x => jsValueToString(x._2))
+      // // We get a unique id for the record
+      // val id = variableIdCounter.getAndIncrement().toString
+      // writer.writeNext((Seq(id) ++ strList)toArray)
+      writer.writeNext((strList)toArray)
     }
+  }
+
+  /* Translates a JSON value to a String that can be insert using COPY statement */
+  private def jsValueToString(x: JsValue) : String = x match {
+    case JsString(x) => x.replace("\\", "\\\\")
+    case JsNumber(x) => x.toString
+    case JsNull => null
+    case JsBoolean(x) => x.toString
+    case JsArray(x) => 
+      val innerData = x.map {
+        case JsString(x) => 
+          val convertedStr = jsValueToString(JsString(x))
+          val escapedStr = convertedStr.replace("\"", "\\\"")
+          s""" "${escapedStr}" """ 
+        case x: JsValue => jsValueToString(x)
+      }.mkString(",")
+      val arrayStr = s"{${innerData}}"
+      arrayStr
+    case x : JsObject => Json.stringify(x)
+    case _ =>
+      log.warning(s"Could not convert JSON value ${x} to String")
+      ""
+  }
 
 }
