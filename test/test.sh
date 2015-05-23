61e9d26f2b920a25abd9fafef29da19f40cf2acc
Jaeho Shin <netj@cs.stanford.edu>
2015-05-23 08:12:39 -0700

    Turns Bats into a submodule

--- test/test.sh
+++ test/test.sh
@@ -5,8 +5,6 @@ cd "$(dirname "$0")"
 
 # make sure bats is available
 PATH="$PWD/bats/bin:$PATH"
-type bats &>/dev/null ||
-    git clone https://github.com/sstephenson/bats.git
 
 # instantiate bats tests templates under its directory
 for t in *.bats.template; do
