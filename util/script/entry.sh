f4faba129b2c8dc49091ed0bc8b7a9781e71555d
Raphael Hoffmann <raphael.hoffmann@gmail.com>
2015-05-29 19:53:15 -0700

    add curl bash entry

--- util/script/entry.sh
+++ util/script/entry.sh
@@ -1,2 +1,12 @@
-#!/bin/bash URL=https://raw.githubusercontent.com/hazyresearch/deepdive/raphael-script/util/script/install.sh function download { scratch=$(mktemp -d -t tmp.XXXXXXXXXX) || exit script_file=$scratch/install.sh echo "Downloading DeepDive installer: $URL" curl -# $URL > $script_file || exit chmod 755 $script_file echo "Running installer from: $script_file" $script_file } download < /dev/tty
+#!/bin/bash
+URL=https://raw.githubusercontent.com/hazyresearch/deepdive/raphael-script/util/script/install.sh
+function download {
+  scratch=$(mktemp -d -t tmp.XXXXXXXXXX) || exit
+  script_file=$scratch/install.sh
+  curl -s $URL > $script_file || exit
+  chmod 755 $script_file
+  echo "Running DeepDive installer from: $script_file"
+  $script_file
+}
+download < /dev/tty
 
