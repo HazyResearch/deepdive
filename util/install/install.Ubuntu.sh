abd07a7f4601fa597ca2c312a3cf99157b6645d6
Jaeho Shin <netj@cs.stanford.edu>
2015-05-30 02:51:43 -0700

    Keeps only essential dependencies for DeepDive on Ubuntu

--- util/install/install.Ubuntu.sh
+++ util/install/install.Ubuntu.sh
@@ -17,31 +17,37 @@ install_runtime_deps() {
     # Installation of the dependencies for the DeepDive stack.
     # (deepdive, sampler, mindbender)
 
-    sudo apt-get update
+    # Many dependencies are already available in TravisCI
+    if [ -z "$TRAVIS" ]; then
+        sudo apt-get update
 
-    # automate license agreement for oracle java
-    echo debconf shared/accepted-oracle-license-v1-1 select true |
-    sudo debconf-set-selections
-    echo debconf shared/accepted-oracle-license-v1-1 seen true |
-    sudo debconf-set-selections
+        # automate license agreement for oracle java
+        echo debconf shared/accepted-oracle-license-v1-1 select true |
+        sudo debconf-set-selections
+        echo debconf shared/accepted-oracle-license-v1-1 seen true |
+        sudo debconf-set-selections
 
-    # install oracle java
-    sudo apt-get install -y python-software-properties
-    sudo add-apt-repository -y ppa:webupd8team/java
-    sudo apt-get update
-    sudo apt-get install -y oracle-java8-installer 
+        # install oracle java
+        sudo apt-get install -y python-software-properties
+        sudo add-apt-repository -y ppa:webupd8team/java
+        sudo apt-get update
+        sudo apt-get install -y oracle-java8-installer 
+
+        # install dependencies for deepdive
+        sudo apt-get install -y make
+    fi
 
     # install additional packages for deepdive
-    sudo apt-get install -y make gnuplot awscli unzip bc
+    sudo apt-get install -y gnuplot unzip bc
 
     # install additional packages for sampler
-    sudo apt-get install -y g++ cmake libnuma-dev libtclap-dev
+    #sudo apt-get install -y g++ cmake libnuma-dev libtclap-dev
 
     # install additional packages for mindbender
-    sudo apt-get install -y libyaml-dev python-pip
+    #sudo apt-get install -y libyaml-dev python-pip
 
     # install performance analysis tools
-    sudo apt-get install -y sysstat
+    #sudo apt-get install -y sysstat
 }
 
 install_Postgres_XL() {
