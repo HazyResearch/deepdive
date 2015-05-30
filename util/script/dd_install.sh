fcdd51db40989114dec7d546151b436b7a13f322
Raphael Hoffmann <raphael.hoffmann@gmail.com>
2015-05-29 19:31:42 -0700

    cleanup install scripts

--- util/script/dd_install.sh
+++ util/script/dd_install.sh
@@ -30,3 +30,18 @@ sudo apt-get install -y unzip
 # install performance analysis tools
 sudo apt-get install -y sysstat
 
+cd ~
+git clone https://github.com/hazyresearch/deepdive.git
+cd deepdive
+make
+
+cd ~
+git clone https://github.com/hazyresearch/sampler.git
+cd sampler
+make
+
+git clone https://github.com/hazyresearch/mindbender.git
+cd mindbender
+make
+# Note: requires user input
+
