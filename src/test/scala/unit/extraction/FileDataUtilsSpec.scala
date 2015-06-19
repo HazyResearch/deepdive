304eb44cd12a123ac33c9b51ab01357226746e6b
alldefector <niufeng14@gmail.com>
2015-06-19 01:54:08 -0700

    Add tests for FileDataUtils and Helpers

--- src/test/scala/unit/extraction/FileDataUtilsSpec.scala
+++ src/test/scala/unit/extraction/FileDataUtilsSpec.scala
@@ -1,5 +1,6 @@
 package org.deepdive.test.unit
 
+import java.io.File
 import org.scalatest._
 import org.deepdive.datastore._
 import play.api.libs.json._
@@ -20,6 +21,29 @@ class FileDataUtilsSpec extends FunSpec {
       }
     }
 
+  }
+
+  describe("Recursively listing a directory") {
+    it("should work") {
+      val dirPath = getClass.getResource("/files/source").getPath
+      val files = FileDataUtils.recursiveListFiles(new File(dirPath))
+      assert(files.length == 4)
+      assert(files.exists{ f =>
+        f.toString.endsWith("convert.py")
+      })
+      assert(files.exists{ g =>
+        g.toString.endsWith("subdir")
+      })
+    }
+  }
+
+  describe("Zipping a directory") {
+    it("should work with subdirectories, empty files, and binary files") {
+      val dirPath = getClass.getResource("/files/source").getPath
+      val blob = FileDataUtils.zipDir(dirPath)
+      assert(blob.length == 749)
+      // TODO: verify zip entries
+    }
   }
 
 }
