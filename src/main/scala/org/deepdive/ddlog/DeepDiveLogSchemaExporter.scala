8a6bd94fc45229697280e578f67f54e903b7bc4a
Jaeho Shin <netj@cs.stanford.edu>
2015-07-16 04:16:31 -0700

    Enhances export-schema to avoid producing empty fields
    e.g., empty annotations, empty args for annotations
    
    Closes #35

--- src/main/scala/org/deepdive/ddlog/DeepDiveLogSchemaExporter.scala
+++ src/main/scala/org/deepdive/ddlog/DeepDiveLogSchemaExporter.scala
@@ -8,33 +8,53 @@ import scala.util.parsing.json.{JSONArray, JSONObject}
 // Schema exporter that dumps column names and types of every relation as well as their annotations
 object DeepDiveLogSchemaExporter extends DeepDiveLogHandler {
 
+  def jsonMap(kvs: (String, Any)*): Map[String, Any] = kvs toMap
+
   def export(decl: SchemaDeclaration): (String, JSONObject) = {
-    val schema = JSONObject(Map(
-      "columns" -> JSONObject(Map() ++ decl.a.terms.zipWithIndex map {
-        case (name, i) => (name, JSONObject(Map(
-          "type" -> decl.a.types(i),
-          "annotations" -> exportAnnotations(decl.a.annotations(i))
-        )))
-      }),
-      "annotations" -> exportAnnotations(decl.annotation)
-    ))
-    (decl.a.name, schema)
+    var schema = jsonMap(
+//      "name" -> decl.a.name // XXX redundant but potentially useful
+    )
+    // column names with types and annotations
+    schema += "columns" -> JSONObject(
+      decl.a.terms.zipWithIndex map {
+        case (name, i) =>
+          var columnSchema = jsonMap(
+//              "name" -> name, // XXX redundant but potentially useful
+              "type" -> decl.a.types(i)
+          )
+          // column annotations are omitted when not present
+          val annos = decl.a.annotations(i)
+          if (annos nonEmpty)
+            columnSchema += "annotations" -> exportAnnotations(annos)
+
+          (name, JSONObject(columnSchema))
+      } toMap)
+    // relation annotations are omitted when not present
+    if (decl.annotation nonEmpty)
+      schema += "annotations" -> exportAnnotations(decl.annotation)
+
+    (decl.a.name, JSONObject(schema))
   }
 
   def exportAnnotations(annos: Seq[Annotation]): JSONArray =
     JSONArray(annos map export toList)
 
-  def export(anno: Annotation): JSONObject =
-    JSONObject(Map(
-        "name" -> anno.name,
-        "args" -> JSONObject(anno.args)
-      ))
+  def export(anno: Annotation): JSONObject = {
+    var a = jsonMap(
+      "name" -> anno.name
+    )
+    if (anno.args nonEmpty)
+      a += "args" -> JSONObject(anno.args)
+    JSONObject(a)
+  }
 
   override def run(parsedProgram: DeepDiveLog.Program, config: DeepDiveLog.Config) = {
     val program = parsedProgram  // TODO derive the program based on config.mode?
-    val relations = program collect {
-      case decl: SchemaDeclaration => export(decl)
-    }
-    println(JSONObject(Map() ++ relations))
+
+    println(JSONObject(Map(
+      "relations" -> JSONObject(program collect {
+          case decl: SchemaDeclaration => export(decl)
+        } toMap)
+    )))
   }
 }
