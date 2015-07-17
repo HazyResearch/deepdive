5fd7dd1eb217a5bf34c6ace2207492205c918dca
Jaeho Shin <netj@cs.stanford.edu>
2015-07-17 03:01:45 -0700

    Settings formatting cleanup

--- src/main/scala/org/deepdive/settings/Settings.scala
+++ src/main/scala/org/deepdive/settings/Settings.scala
@@ -26,12 +26,11 @@ case class Settings(
 }
 
 /* Calibration Settings */
-case class CalibrationSettings(holdoutFraction: Double = 0.0,
-                               holdoutQuery: Option[String] = None,
-                               observationQuery: Option[String] = None)
-
-/* Database connection specifie in the settings */
-case class Connection(url: String, user: String, password: String)
+case class CalibrationSettings(
+  holdoutFraction: Double = 0.0,
+  holdoutQuery: Option[String] = None,
+  observationQuery: Option[String] = None
+)
 
 object IncrementalMode extends Enumeration {
   type IncrementalMode = Value
@@ -92,8 +91,13 @@ case class Extractor(
 )
 
 /* A Factor specified in the settings */
-case class FactorDesc(name: String, inputQuery: String, func: FactorFunction,
-  weight: FactorWeight, weightPrefix: String)
+case class FactorDesc(
+  name: String,
+  inputQuery: String,
+  func: FactorFunction,
+  weight: FactorWeight,
+  weightPrefix: String
+)
 
 /* Factor Weight for a factor specified in the settings*/
 sealed trait FactorWeight {
@@ -114,60 +118,47 @@ import scala.language.implicitConversions
 sealed trait FactorFunction {
   def variables : Seq[FactorFunctionVariable]
   /* Data type can be one of: Boolean, Discrete, Continuous */
-  def variableDataType : String = "Boolean"
+  def variableDataType : String
   /* The relations used in this factor function */
   def relations = variables.map(_.relation).toSet
 }
 
-/* A factor function of fom A and B and C ... -> Z */
-case class ImplyFactorFunction(variables: Seq[FactorFunctionVariable]) extends FactorFunction {
-  override def variableDataType = "Boolean"
+abstract class BooleanFactorFunction extends FactorFunction {
+  override def variableDataType: String = "Boolean"
 }
+abstract class DiscreteFactorFunction extends FactorFunction {
+  override def variableDataType: String = "Discrete"
+}
+
+/* A factor function of fom A and B and C ... -> Z */
+case class ImplyFactorFunction(variables: Seq[FactorFunctionVariable]) extends BooleanFactorFunction
 
 /* A factor function describing A or B or C ... */
-case class OrFactorFunction(variables: Seq[FactorFunctionVariable]) extends FactorFunction {
-  override def variableDataType = "Boolean"
-}
+case class OrFactorFunction(variables: Seq[FactorFunctionVariable]) extends BooleanFactorFunction
 
 /* A factor function describing A and B and C ... */
-case class AndFactorFunction(variables: Seq[FactorFunctionVariable]) extends FactorFunction {
-  override def variableDataType = "Boolean"
-}
+case class AndFactorFunction(variables: Seq[FactorFunctionVariable]) extends BooleanFactorFunction
 
 /* A factor function describing A == B. Restricted to two variables. */
-case class EqualFactorFunction(variables: Seq[FactorFunctionVariable]) extends FactorFunction {
-  override def variableDataType = "Boolean"
-}
+case class EqualFactorFunction(variables: Seq[FactorFunctionVariable]) extends BooleanFactorFunction
 
 /* A factor function describing A == True. Restricted to one variable. */
-case class IsTrueFactorFunction(variables: Seq[FactorFunctionVariable]) extends FactorFunction {
-  override def variableDataType = "Boolean"
-}
+case class IsTrueFactorFunction(variables: Seq[FactorFunctionVariable]) extends BooleanFactorFunction
 
 /* A factor function describing XOR(A,B,C,...) */
-case class XorFactorFunction(variables: Seq[FactorFunctionVariable]) extends FactorFunction {
-  override def variableDataType = "Boolean"
-}
+case class XorFactorFunction(variables: Seq[FactorFunctionVariable]) extends BooleanFactorFunction
 
 /* A factor function describing and between all combinations of values for multinomial variables */
-case class MultinomialFactorFunction(variables: Seq[FactorFunctionVariable]) extends FactorFunction {
-  override def variableDataType = "Discrete"
-}
+case class MultinomialFactorFunction(variables: Seq[FactorFunctionVariable]) extends DiscreteFactorFunction
 
 /* A factor function describing linear semantics, (A -> Z) + (B -> Z) + (C -> Z). */
-case class LinearFactorFunction(variables: Seq[FactorFunctionVariable]) extends FactorFunction {
-  override def variableDataType = "Boolean"
-}
+case class LinearFactorFunction(variables: Seq[FactorFunctionVariable]) extends BooleanFactorFunction
 
 /* A factor function describing ratio semantics, log(1 + (A -> Z) + (B -> Z) + (C -> Z)). */
-case class RatioFactorFunction(variables: Seq[FactorFunctionVariable]) extends FactorFunction {
-  override def variableDataType = "Boolean"
-}
+case class RatioFactorFunction(variables: Seq[FactorFunctionVariable]) extends BooleanFactorFunction
 
 /* A factor function describing logical semantics, (A -> Z) or (B -> Z) or (C -> Z)*/
-case class LogicalFactorFunction(variables: Seq[FactorFunctionVariable]) extends FactorFunction {
-  override def variableDataType = "Boolean"
-}
+case class LogicalFactorFunction(variables: Seq[FactorFunctionVariable]) extends BooleanFactorFunction
 
 /* A variable used in a Factor function */
 case class FactorFunctionVariable(relation: String, field: String, isArray: Boolean = false,
@@ -184,8 +175,13 @@ object FactorFunctionVariable {
   }
 }
 
-case class InferenceSettings(factors: List[FactorDesc] = Nil, insertBatchSize: Option[Int] = None,
-  skipLearning: Boolean = false, weightTable: String = "", parallelGrounding: Boolean = false)
+case class InferenceSettings(
+  factors: List[FactorDesc] = Nil,
+  insertBatchSize: Option[Int] = None,
+  skipLearning: Boolean = false,
+  weightTable: String = "",
+  parallelGrounding: Boolean = false
+)
 
 import scala.language.implicitConversions
 
@@ -202,53 +198,46 @@ object InputQuery {
 
 /* Extractor specified in the settings */
 case class LoaderConfig (
-    connection: String,
-    schemaFile: String,
-    threads: Int,
-    parallelTransactions: Int
+  connection: String,
+  schemaFile: String,
+  threads: Int,
+  parallelTransactions: Int
 )
 
 /* A DeepDive pipeline that specifies which extractors and factor tasks should be executed */
 case class Pipeline(id: String, tasks: Set[String])
 
 /* User settings pipelines */
-case class PipelineSettings(activePipelineName: Option[String] = None,
-                            pipelines: List[Pipeline] = List.empty,
-                            relearnFrom: String = null, // TODO Option
-                            baseDir: Option[String] = None) {
-  def activePipeline : Option[Pipeline] = activePipelineName.flatMap { name =>
-    pipelines.find(_.id == name)
+case class PipelineSettings(
+  activePipelineName: Option[String] = None,
+  pipelines: List[Pipeline] = List.empty,
+  relearnFrom: String = null, // TODO Option
+  baseDir: Option[String] = None
+) {
+  def activePipeline: Option[Pipeline] = activePipelineName flatMap { name =>
+    pipelines find (_.id == name)
   }
 }
 
 /* Calibration Settings */
-case class SamplerSettings(samplerCmd: String = null,
-                           samplerArgs: String = "")
+case class SamplerSettings(
+  samplerCmd: String = null,
+  samplerArgs: String = ""
+)
 
 /* Schema Settings */
-case class SchemaSettings(variables: Map[String, _ <: VariableDataType] = Map.empty,
-                          setupFile: Option[String] = None)
-
-// sealed trait VariableDataType {
-//   def cardinality: Option[Long]
-// }
-// case object BooleanType extends VariableDataType {
-//   def cardinality = None
-//   override def toString() = "Boolean"
-// }
-// case class MultinomialType(numCategories: Int) extends VariableDataType {
-//   def cardinality = Some(numCategories)
-//   override def toString() = "Multinomial"
-// }
+case class SchemaSettings(
+  variables: Map[String, _ <: VariableDataType] = Map.empty,
+  setupFile: Option[String] = None
+)
 
 sealed trait VariableDataType {
-  def cardinality: Long
+  def cardinality: Int
 }
 case object BooleanType extends VariableDataType {
   def cardinality = 2
   override def toString() = "Boolean"
 }
-case class MultinomialType(numCategories: Int) extends VariableDataType {
-  def cardinality = numCategories
-  override def toString() = "Multinomial"
+case class MultinomialType(cardinality: Int) extends VariableDataType {
+  override def toString() = s"Multinomial(${cardinality})"
 }
