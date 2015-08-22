d109bb28ab47b0633bbafb5fdc1cdba8ec83a368
Jaeho Shin <netj@cs.stanford.edu>
2015-08-21 23:09:13 -0700

    Fixes testability checker to not silence errors

--- test/mysql/should-work.sh
+++ test/mysql/should-work.sh
@@ -2,10 +2,9 @@
 # A script to test whether the MySQL tests should be done or not
 set -eu
 
+cd "$(dirname "$0")"
+. ./env.sh
 {
-    cd "$(dirname "$0")"
-    . ./env.sh
-
     # try executing a SQL query against the configured database for tests
     DBNAME=mysql db-execute "SELECT VERSION()"
 } &>/dev/null
