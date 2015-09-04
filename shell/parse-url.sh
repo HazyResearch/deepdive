b5fd72543cfd850058ed0c50d0034823151a541d
senwu <senwu@stanford.edu>
2015-09-04 01:06:34 -0700

    support ssl connection   Typical jdbc connection string for connecting to a postgres:   jdbc:postgresql://localhost:5432/dbname
      In order to support ssl connections. You can specify this in the
      connection string with:
      jdbc:postgresql://localhost:5432/dbname?ssl=true
    
      But what if the ssl certificate is self-signed? You have two options,
      import the self-signed cert into java's keystore for each jvm the
      software will run on or try the non-validating ssl factory:
      jdbc:postgresql://localhost:5432/dbname?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory
    
      In this commit, we only support non-validating ssl factory.

--- shell/parse-url.sh
+++ shell/parse-url.sh
@@ -18,4 +18,7 @@ port=${port#:}
 user=${user_password%:*}
 password=${user_password#$user}
 password=${password#:}
-dbname=${rest}  # TODO do we need to strip any parameters from the rest of the URL?
+dbname=${rest%\?*}
+rest1=${rest#$dbname?}
+ssl=${rest1#ssl=} # TODO we only support ssl parameter from the rest of the URL
+#dbname=${rest}  # TODO do we need to strip any parameters from the rest of the URL?
