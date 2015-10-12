fedcac848197f8e2bf88aee8631a18d369542fcb
Jaeho Shin <netj@cs.stanford.edu>
2015-10-12 02:19:33 -0700

    Changes compiled extractor/factor names to be readable

--- src/main/scala/org/deepdive/ddlog/DeepDiveLogCompiler.scala
+++ src/main/scala/org/deepdive/ddlog/DeepDiveLogCompiler.scala
@@ -136,13 +136,48 @@ class CompilationState( statements : DeepDiveLog.Program, config : DeepDiveLog.C
     throw new RuntimeException(message)
   }
 
+  def optionalIndex(idx: Int) =
+    if (idx < 1) "" else s"${idx}"
+
   // Given a statement, resolve its name for the compiled extractor block.
-  def resolveExtractorBlockName(s: Statement): String = s"extraction_rule_${statements indexOf s}"
+  def resolveExtractorBlockName(s: Statement): String = s match {
+    case d: SchemaDeclaration => s"init_${d.a.name}"
+    case e: ExtractionRule => s"ext_${e.headName}"
+    case f: FunctionCallRule => s"ext${
+      // Extra numbering is necessary because there can be many function calls to the same head relation
+      // XXX This comes right after the prefix to prevent potential collision with user's name
+      val functionCallsForSameOutputAndFunction =
+        (functionCallRuleGroupByOutput getOrElse(f.output, Set.empty) toList).filter (_.function == f.function)
+      val idx = functionCallsForSameOutputAndFunction indexOf f
+      optionalIndex(idx)
+    }_${f.output}_by_${f.function}"
+    case _ => sys.error(s"${s}: Cannot determine extractor block name for rule")
+  }
 
   // Given an inference rule, resolve its name for the compiled inference block.
-  def resolveInferenceBlockName(s: InferenceRule): String = mode match {
-    case INCREMENTAL => s"dd_delta_inference_rule_${inferenceRules indexOf s}"
-    case _ =>           s"inference_rule_${inferenceRules indexOf s}"
+  def resolveInferenceBlockName(s: InferenceRule): String = {
+    val factorFuncName: String = s.head.function.toString.toLowerCase
+    val idxInInferenceRulesSharingHead = inferenceRules filter(_.head equals s.head) indexOf(s)
+    mode match {
+      case INCREMENTAL => s"dd_delta_inf${
+        // XXX This comes right after the prefix to prevent potential collision with user's name
+        optionalIndex(idxInInferenceRulesSharingHead)
+      }_${
+        factorFuncName
+      }_${
+        s.head.terms map {_.name stripPrefix("dd_new_")} mkString("_")
+      }"
+      case _ =>           s"inf${
+        // XXX This comes right after the prefix to prevent potential collision with user's name
+        optionalIndex(idxInInferenceRulesSharingHead)
+      }_${
+        // function name
+        factorFuncName
+      }_${
+        // followed by variable names
+        s.head.terms map {_.name} mkString("_")
+      }"
+    }
   }
 
   // Given a variable, resolve it.  TODO: This should give a warning,
