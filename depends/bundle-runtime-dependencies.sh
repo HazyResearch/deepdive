96c3294db85f31ac22f3e99fa62c6c20fe608b4a
Jaeho Shin <netj@cs.stanford.edu>
2015-11-19 09:55:38 -0800

    Turns buildkit into a submodule
    that's used for bundling runtime dependencies

--- depends/bundle-runtime-dependencies.sh
+++ depends/bundle-runtime-dependencies.sh
@@ -5,7 +5,6 @@ set -eu
 cd "$(dirname "$0")"
 
 mkdir -p .build
-[[ -d .build/buildkit ]] ||
-    git clone https://github.com/netj/buildkit.git .build/buildkit
-PATH="$PWD"/.build/buildkit:"$PATH"
-.build/buildkit/depends/module.build
+[[ -e buildkit/.git ]] || git submodule update --init buildkit
+PATH="$PWD"/buildkit:"$PATH"
+buildkit/depends/module.build
