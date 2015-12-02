afd9255b80caaefa1eca9b7f81d1869d16a3ce3f
Jaeho Shin <netj@cs.stanford.edu>
2015-12-01 18:16:00 -0800

    Adds a sanity check for tests to support running .bats files directly
    e.g., for unit tests

--- test/env.sh
+++ test/env.sh
@@ -8,6 +8,17 @@ DEEPDIVE_SOURCE_ROOT=$(cd "$DEEPDIVE_TEST_ROOT/.." && pwd)
 # determine the DeepDive installation to run tests against (defaults to the staged one)
 : ${DEEPDIVE_HOME:=$(cd "$DEEPDIVE_SOURCE_ROOT" && echo "$PWD"/dist/stage)}
 
+# make sure there's actually DeepDive to test against
+# TODO more sophisticated detection
+[[ -d "$DEEPDIVE_HOME" ]] || {
+    echo >&2 "$DEEPDIVE_HOME: No DeepDive installation found."
+    case $DEEPDIVE_HOME in
+        */dist/stage)
+            echo >&2 'Did you forget to run `make test-build` first?'
+    esac
+    false
+}
+
 # configure PATH and CLASSPATH for tests
 PATH="$DEEPDIVE_HOME/util:$DEEPDIVE_HOME/bin:$PATH"
 ! [[ -r "$DEEPDIVE_HOME"/env.sh ]] || source "$DEEPDIVE_HOME"/env.sh
