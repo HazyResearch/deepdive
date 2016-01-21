cfdfa39dfdb22a6754cd84c2721095c1a6c45328
Ivan Gozali <igozali@stanford.edu>
2016-01-20 21:42:06 -0800

    Very rough first draft of compute-execute for Torque schedulers
    Signed-off-by: Ivan Gozali <igozali@stanford.edu>

--- stage.sh
+++ stage.sh
@@ -94,6 +94,7 @@ stage runner/ps_descendants                                       util/
 stage runner/deepdive-compute                                     util/
 stage runner/load-compute-driver.sh                               util/
 stage runner/compute-driver/local                                 util/compute-driver/
+stage runner/compute-driver/torque                                util/compute-driver/
 stage runner/computers-default.conf                               util/
 stage .build/submodule/runner/mkmimo/mkmimo                       util/
 
