c594512c7cc41cffc2fd7cbb50d2a23d0222eae4
Jaeho Shin <netj@cs.stanford.edu>
2016-02-01 22:03:39 -0800

    Fixes spouse example removing the absolute path to corpus
    and fixing an incorrect supervision rule.

--- examples/spouse/input/articles.tsv.sh
+++ examples/spouse/input/articles.tsv.sh
@@ -2,7 +2,14 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-cat /dfs/scratch0/thodrek/signalmedia/signalmedia-1m.jsonl |
+corpus=signalmedia/signalmedia-1m.jsonl
+[[ -e "$corpus" ]] || {
+    echo "Missing $PWD/$corpus"
+    echo "Please Download it from http://research.signalmedia.co/newsir16/signal-dataset.html"
+    false
+} >&2
+
+cat "$corpus" |
 grep -E 'wife|husband|married' |
 head -100 |
 jq -r '[.id, .content | gsub("[\r]"; "")] | @tsv'
