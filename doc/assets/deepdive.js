0dfecac32f19fa8332297f066788705277509d62
Jaeho Shin <netj@cs.stanford.edu>
2016-02-09 03:31:11 -0800

    Fixes CSS/JS for bash prompt styling to accept multi lines with leading whitespaces

--- doc/assets/deepdive.js
+++ doc/assets/deepdive.js
@@ -8,6 +8,14 @@
 $(function(){
     /* prevent bash code blocks with multiple lines from triggering a CSS style that adds a prompt prefix */
     $("code.language-bash")
-        .filter(function(){ return $(this).text().trim().indexOf("\n") > -1; })
-        .addClass("multi-line");
+        .filter(function(){
+            var text = $(this).text().trim();
+            return (
+                // multiple lines
+                text.indexOf("\n") > -1 &&
+                // with some lines not beginning with whitespace
+                ! text.split("\n").slice(1).every(function(l){ return /^\s|^$/.test(l); })
+            );
+        })
+        .addClass("multi-commands");
 });
