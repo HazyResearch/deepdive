41b4da9306831be2de45cd750ae1ce6ea0c97c34
Jaeho Shin <netj@cs.stanford.edu>
2016-02-09 03:10:51 -0800

    Tweaks website CSS/JS to render a prompt dollar sign before single line commands

--- doc/assets/deepdive.js
+++ doc/assets/deepdive.js
@@ -4,3 +4,10 @@
 {% include js/jquery-1.10.1.min.js %}
 {% include js/bootstrap.min.js %}
 {% include js/toc.js %}
+
+$(function(){
+    /* prevent bash code blocks with multiple lines from triggering a CSS style that adds a prompt prefix */
+    $("code.language-bash")
+        .filter(function(){ return $(this).text().trim().indexOf("\n") > -1; })
+        .addClass("multi-line");
+});
