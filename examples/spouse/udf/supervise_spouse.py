9b3afead8b16d2e098fc637f4eb8c5dd72fb6b25
Jaeho Shin <netj@cs.stanford.edu>
2016-02-10 01:45:45 -0800

    Fixes trailing whitespace

--- examples/spouse/udf/supervise_spouse.py
+++ examples/spouse/udf/supervise_spouse.py
@@ -25,7 +25,7 @@ def supervise(
     MARRIED = frozenset(["wife", "husband"])
     FAMILY = frozenset(["mother", "father", "sister", "brother", "brother-in-law"])
     MAX_DIST = 10
-    
+
     # Common data objects
     p1_end_idx = min(p1_end, p2_end)
     p2_start_idx = max(p1_begin, p2_begin)
@@ -34,25 +34,25 @@ def supervise(
     intermediate_ner_tags = ner_tags[p1_end_idx+1:p2_start_idx]
     tail_lemmas = lemmas[p2_end_idx+1:]
     spouse = SpouseLabel(p1_id=p1_id, p2_id=p2_id, label=None, type=None)
-    
+
     # Rule: Candidates that are too far apart
     if len(intermediate_lemmas) > MAX_DIST:
         yield spouse._replace(label=-1, type='neg:far_apart')
-    
+
     # Rule: Candidates that have a third person in between
     if 'PERSON' in intermediate_ner_tags:
         yield spouse._replace(label=-1, type='neg:third_person_between')
-    
+
     # Rule: Sentences that contain wife/husband in between
     #         (<P1>)([ A-Za-z]+)(wife|husband)([ A-Za-z]+)(<P2>)
     if len(MARRIED.intersection(intermediate_lemmas)) > 0:
         yield spouse._replace(label=1, type='pos:wife_husband_between')
-    
+
     # Rule: Sentences that contain and ... married
     #         (<P1>)(and)?(<P2>)([ A-Za-z]+)(married)
     if ("and" in intermediate_lemmas) and ("married" in tail_lemmas):
         yield spouse._replace(label=1, type='pos:married_after')
-    
+
     # Rule: Sentences that contain familial relations:
     #         (<P1>)([ A-Za-z]+)(brother|stster|father|mother)([ A-Za-z]+)(<P2>)
     if len(FAMILY.intersection(intermediate_lemmas)) > 0:
