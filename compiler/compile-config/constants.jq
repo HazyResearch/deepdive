HazyResearch/deepdive

commit b78ee3c064f72a0cfec3a5fa70a014847d6f899b

Rename dd_query to dd_factors and update internal tables doc

compiler/compile-config/constants.jq
@@ -6,7 +6,7 @@ def deepdiveVariableIdColumn                :  "id"                             
 def deepdiveGlobalHoldoutTable              :  "dd_graph_variables_holdout"     ;
 def deepdiveGlobalObservationTable          :  "dd_graph_variables_observation" ;
 def deepdiveGlobalWeightsTable              :  "dd_graph_weights"               ;
-def deepdivePrefixForFactorsTable           :  "dd_query_"                      ;  # TODO correct prefix to dd_factors_?
+def deepdivePrefixForFactorsTable           :  "dd_factors_                     ;
 def deepdivePrefixForWeightsTable           :  "dd_weights_"                    ;
 def deepdivePrefixForMultinomialWeightsTable:  "dd_weightsmulti_"               ;
 def deepdivePrefixForVariableCategoriesTable:  "dd_categories_"                 ;

commit 85b2d6021ac6d24cba96183a17d3f1d66b18ad96

Corrects dd_factor_ rename error in compiler
introduced in b78ee3c064f72a0cfec3a5fa70a014847d6f899b

compiler/compile-config/constants.jq
@@ -6,7 +6,7 @@ def deepdiveVariableIdColumn                :  "id"                             
 def deepdiveGlobalHoldoutTable              :  "dd_graph_variables_holdout"     ;
 def deepdiveGlobalObservationTable          :  "dd_graph_variables_observation" ;
 def deepdiveGlobalWeightsTable              :  "dd_graph_weights"               ;
-def deepdivePrefixForFactorsTable           :  "dd_factors_                     ;
+def deepdivePrefixForFactorsTable           :  "dd_factors_"                    ;
 def deepdivePrefixForWeightsTable           :  "dd_weights_"                    ;
 def deepdivePrefixForMultinomialWeightsTable:  "dd_weightsmulti_"               ;
 def deepdivePrefixForVariableCategoriesTable:  "dd_categories_"                 ;