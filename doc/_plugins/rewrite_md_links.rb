db37088a30964b9e91f854b42d297fb74bb67556
Jaeho Shin <netj@cs.stanford.edu>
2016-02-11 21:43:20 -0800

    Fixes doc config to work with ruby < 2.0 and jekyll < 3.x

--- doc/_plugins/rewrite_md_links.rb
+++ doc/_plugins/rewrite_md_links.rb
@@ -1,5 +1,20 @@
 # A plugin to rewrite GitHub-friendly .md links to ones without the extension for the website
-# See: http://stackoverflow.com/a/29234076
-Jekyll::Hooks.register :pages, :pre_render do |page|
-    page.content = page.content.gsub(/(\[[^\]]*\]\([^:\)]*)\.md(#.*)?\)/, '\1\2)')
+begin
+    # jekyll >= 3.x
+    Jekyll::Hooks.register :pages, :pre_render do |page|
+        page.content = page.content.gsub(/(\[[^\]]*\]\([^:\)]*)\.md(#.*)?\)/, '\1\2)')
+    end
+rescue
+    # jekyll <= 2.x
+    # See: http://stackoverflow.com/a/29234076
+    module ChangeLocalMdLinksToHtml
+        class Generator < Jekyll::Generator
+            def generate(site)
+                site.pages.each { |p| rewrite_links(site, p) }
+            end
+            def rewrite_links(site, page)
+                page.content = page.content.gsub(/(\[[^\]]*\]\([^:\)]*)\.md(#.*)?\)/, '\1\2)')
+            end
+        end
+    end
 end
