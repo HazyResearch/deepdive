a647aed4611c126e5793db23ba4df66f892d023b
Jaeho Shin <netj@cs.stanford.edu>
2016-02-14 12:21:45 -0800

    Fixes .md link rewriter plugin to work with `{% include %}` as well

--- doc/_plugins/rewrite_md_links.rb
+++ doc/_plugins/rewrite_md_links.rb
@@ -1,8 +1,15 @@
 # A plugin to rewrite GitHub-friendly .md links to ones without the extension for the website
 begin
     # jekyll >= 3.x
-    Jekyll::Hooks.register :pages, :pre_render do |page|
-        page.content = page.content.gsub(/(\[[^\]]*\]\([^:\)]*)\.md(#[^)]*)?\)/, '\1\2)')
+    Jekyll::Hooks.register :pages, :pre_render, priority: :lowest do |page|
+        # manipulate Markdown links to drop .md extensions
+        #STDERR.puts "rewrite_md_links.rb: pre_render #{page.url}" if page.content.match(/(\[[^\]]*\]\([^:\)]*)\.md(#[^\)]*)?\)/)
+        page.content = page.content.gsub(/(\[[^\]]*\]\([^:\)]*)\.md(#[^\)]*)?\)/, '\1\2)')
+    end
+    Jekyll::Hooks.register :pages, :post_render do |page|
+        # need to manipulate HTML links as well because Markdown from {% include ... %} don't go through :pre_render
+        #STDERR.puts "rewrite_md_links.rb: post_render #{page.url}" if page.output.match(/href="([^":]+)\.md(#[^"]*)?"/)
+        page.output = page.output.gsub(/href="([^":]+)\.md(#[^"]*)?"/, 'href="\1\2"')
     end
 rescue
     # jekyll <= 2.x
