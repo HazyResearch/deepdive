03e99194c6dc341b160aeca799cae2f292fcd961
Feiran Wang <feiran.wang1@gmail.com>
2016-02-14 17:23:22 -0800

    Add comment

--- src/main/scala/org/deepdive/ddlog/DeepDiveLogCompiler.scala
+++ src/main/scala/org/deepdive/ddlog/DeepDiveLogCompiler.scala
@@ -346,7 +346,7 @@ class QueryCompiler(cq : ConjunctiveQuery, ss: CompilationState) {
       case _ => s" AS column_${cq.headTerms indexOf e}"
     }
     case ViewAlias => s" AS column_${cq.headTerms indexOf e}"
-    case UseVariableAsAlias => s""" AS "${DeepDiveLogPrettyPrinter.print(e)}"""" //"
+    case UseVariableAsAlias => s""" AS "${DeepDiveLogPrettyPrinter.print(e)}"""" //" fix sublime highlighting
   }
 
   // resolve an expression
