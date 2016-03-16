ff01d3bc46bb2df7f5af3b6fddd85ad1881ced19
Jaeho Shin <netj@cs.stanford.edu>
2016-03-16 04:12:02 -0700

    Replaces mkmimo with a dumb-down version on Macs
    as a workaround for #522 and fixes #509

--- runner/mkmimo.build.sh
+++ runner/mkmimo.build.sh
@@ -3,4 +3,10 @@
 set -euo pipefail
 cd "${0%.build.sh}"
 
+# XXX a temporary workaround until mkmimo's crashing issue for Macs
+# running OS X 10.11 El Capitan is completely solved
+case $(uname) in
+    Darwin) export MKMIMO_IMPL=bash
+esac
+
 make
