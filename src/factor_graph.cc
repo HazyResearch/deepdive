3113c6f2286b290b7a0a26d3d5fc035f59fc7469
Jaeho Shin <jaeho.shin@lattice.io>
2016-05-16 23:58:42 -0700

    Drops unused bswap64 macro

--- src/factor_graph.cc
+++ src/factor_graph.cc
@@ -5,13 +5,6 @@
 
 #include <fstream>
 
-// 64-bit big endian to little endian
-#define bswap_64(x)                                                            \
-  ((((x)&0xff00000000000000ull) >> 56) | (((x)&0x00ff000000000000ull) >> 40) | \
-   (((x)&0x0000ff0000000000ull) >> 24) | (((x)&0x000000ff00000000ull) >> 8) |  \
-   (((x)&0x00000000ff000000ull) << 8) | (((x)&0x0000000000ff0000ull) << 24) |  \
-   (((x)&0x000000000000ff00ull) << 40) | (((x)&0x00000000000000ffull) << 56))
-
 // sort according to id
 template <class T>
 class idsorter : public std::binary_function<T, T, bool> {
