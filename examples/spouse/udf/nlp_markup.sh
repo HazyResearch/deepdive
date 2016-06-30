7949aadf079cd773cee4b0763174732d6438ab5d
Jaeho Shin <jaeho.shin@lattice.io>
2016-06-30 15:23:58 -0700

    Finishes first cut of `deepdive corenlp sentences-tsj`
    that can handle results of `deepdive corenlp parse-tsj` to turn raw
    CoreNLP results into more compact representations (aka. "sentence
    tables") in a simple yet generic way for specifying input/output schema
    and projection as command-line arguments.  The syntax allows any fields
    of `tokens` and `*-dependencies` for each sentence in the raw CoreNLP
    results to be easily projected out.
    
    See `util/test/deepdive_corenlp.bats` and
    `example/spouse/udf/nlp_markup.sh` that demonstrates usage.

--- examples/spouse/udf/nlp_markup.sh
+++ examples/spouse/udf/nlp_markup.sh
@@ -6,54 +6,25 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
+# some configuration knobs for CoreNLP
+: ${CORENLP_BASEPORT:=}
+: ${CORENLP_JAVAOPTS:=}
+: ${CORENLP_ANNOTATORS:="
+        tokenize
+        ssplit
+        pos
+        ner
+        lemma
+        depparse
+    "}
+
+export CORENLP_BASEPORT CORENLP_JAVAOPTS
 deepdive corenlp start
+
+export CORENLP_ANNOTATORS
 deepdive corenlp parse-tsj docid+ content=nlp -- docid nlp |
-deepdive corenlp sentences-tsj docid nlp \
-                            -- docid nlp.{index,word,lemma,pos,ner,characterOffsetBegin,dep,governor}
+deepdive corenlp sentences-tsj docid content:nlp \
+                            -- docid nlp.{index,tokens.{word,lemma,pos,ner,characterOffsetBegin}} \
+                                     nlp.collapsed-dependencies.{dep_type,dep_token}
 
-jq -r '
-# TODO doc metadata
-[ .sentences[] |
-[ [.tokens[].word]
-, [.tokens[].lemma]
-, [.tokens[].pos]
-, [.tokens[] | [.characterOffsetBegin,.characterOffsetEnd]]
-] | map(@json) | join("\t")
-'
-
-# doc table
-jq -R -r '
-def withtsv(f) : split("\t") | f | join("\t");
-def withjson(f):    fromjson | f | tojson    ;
-
-withtsv(
-    [ .[0]
-    , .[1] | withjson(
-            .sentences[] |
-                [ [.tokens[].word]
-                , [.tokens[].lemma]
-                , [.tokens[].pos]
-                , [.tokens[] | [.characterOffsetBegin,.characterOffsetEnd]]
-                ]
-        )
-    ]
-)'
-
-# sentences table
-jq -R -r '
-def withtsv(f) : split("\t") | f | join("\t");
-def withjson(f):    fromjson | f | tojson    ;
-
-withtsv(
-    . as $row |
-    .[1] | (fromjson | .sentences[] |
-                [$row[0]] +
-                [ [.tokens[].word]
-                , [.tokens[].lemma]
-                , [.tokens[].pos]
-                , [.tokens[] | [.characterOffsetBegin,.characterOffsetEnd]]
-                | tojson]                                                  
-             )           
-              
-)'
 deepdive corenlp stop
