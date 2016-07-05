24c1c107e201c14bd34b02f1e44477657adef6ee
Jaeho Shin <netj@cs.stanford.edu>
2016-07-05 15:32:23 -0700

    WIP still trying to fix python-virtualenv bootstrapping on Travis
    pip installs locally, but it oddly picks up system's pip, so working
    around it

--- extern/bundled/python-virtualenv/install.sh
+++ extern/bundled/python-virtualenv/install.sh
@@ -5,25 +5,20 @@ unset PYTHONPATH  # existing PYTHONPATH can interfere
 
 ( # install a Python virtualenv
 if ! type virtualenv &>/dev/null; then
-    # if virtualenv isn't available, install it locally with pip
-    [[ -x bootstrap/bin/pip ]] || {
+    # if virtualenv isn't available, install it locally along with pip
+    [[ -e bootstrap/virtualenv.py ]] || {
         curl -RLO https://bootstrap.pypa.io/get-pip.py &&
-        python get-pip.py --ignore-installed --prefix bootstrap
+        python get-pip.py virtualenv --ignore-installed --target bootstrap
     }
-    # set up environment to use for bootstrapping
-    for d in "$PWD"/bootstrap/lib/python*/site-packages; do
-        [[ -d "$d" ]] || continue
-        export PYTHONPATH="$d${PYTHONPATH:+:$PYTHONPATH}"
-    done
-    PATH="$PWD/bootstrap/bin:$PATH"
-    # XXX probing Travis
-    type -a pip
-    which pip
-    pip --version
-    bootstrap/bin/pip --version
-    # install virtualenv locally with the local pip
-    [[ -x bootstrap/bin/virtualenv ]] ||
-        bootstrap/bin/pip install virtualenv --ignore-installed --prefix bootstrap
+    virtualenv() {
+        # use the virtualenv locally installed
+        # XXX --target doesn't give us the handy script while --prefix does, but
+        # since the script oddly doesn't pick up the locally installed pip on
+        # Travis CI, we basically need to explicitly force it here..
+        # See: https://github.com/pypa/pip/pull/3723
+        PYTHONPATH="$PWD/bootstrap" \
+            python -c 'import virtualenv; virtualenv.main()' "$@"
+    }
 fi
 virtualenv --always-copy prefix || virtualenv prefix
 virtualenv --relocatable prefix
