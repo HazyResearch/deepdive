6641a734a086f9e56086751787779ba65ea2c0ad
Jaeho Shin <netj@cs.stanford.edu>
2016-10-26 03:36:59 +0900

    WIP Rewrites conda recipe

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,44 +1,51 @@
-if [ "$(uname)" == "Darwin" ]; then
-  # C++11 finagling for Mac OSX
-  export CC=clang
-  export CXX=clang++
-  export MACOSX_VERSION_MIN="10.9"
-  CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  CXXFLAGS="${CXXFLAGS} -Wno-error=unused-command-line-argument"
-  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export LINKFLAGS="${LDFLAGS}"
-  export MACOSX_DEPLOYMENT_TARGET=10.9
+#!/usr/bin/env bash
+case $(uname) in
+    Darwin)
+        # C++11 finagling for Mac OSX
+        export CC=clang
+        export CXX=clang++
+        export MACOSX_VERSION_MIN="10.9"
+        CXXFLAGS="$CXXFLAGS -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+        CXXFLAGS="$CXXFLAGS -Wno-error=unused-command-line-argument"
+        export LDFLAGS="$LDFLAGS -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+        export LINKFLAGS="$LDFLAGS"
+        export MACOSX_DEPLOYMENT_TARGET=10.9
 
-  # make sure clang-format is installed
-  # e.g. brew install clang-format
+        # make sure clang-format is installed
+        # e.g. brew install clang-format
 
-  # make sure autoreconf can be found
-  # e.g. brew install autoconf
+        # make sure autoreconf can be found
+        # e.g. brew install autoconf
 
-  # for graphviz, also brew install autogen libtool
-fi
+        # for graphviz, also brew install autogen libtool
+        ;;
+esac
 
+# we'll rely on conda for all runtime dependencies
 make no-bundled-runtime-dependencies
-make install PREFIX=$PREFIX
 
-# add the /util directory to the PATH inside this conda env
-# http://conda.pydata.org/docs/using/envs.html#saved-environment-variables
-cat << EOF > ${PKG_NAME}-env-activate.sh
-#!/usr/bin/env bash
+# build and install under a deepdive subdir in conda prefix
+make install PREFIX="$PREFIX/$PKG_NAME"
 
-export PRE_${PKG_NAME}_PATH=\$PATH
-export PATH=\$CONDA_PREFIX/util:\$PATH
-EOF
+# expose executables in bin/
+for cmd in "$PREFIX/$PKG_NAME"/bin/*; do
+    [[ -x "$cmd" ]] || continue
+    ln -sfnr "$cmd" "$PREFIX"/bin/
+done
 
-cat << EOF > ${PKG_NAME}-env-deactivate.sh
-#!/usr/bin/env bash
 
-export PATH=\$PRE_${PKG_NAME}_PATH
-unset PRE_${PKG_NAME}_PATH
-EOF
-
-mkdir -p $PREFIX/etc/conda/activate.d
-mkdir -p $PREFIX/etc/conda/deactivate.d
-
-mv ${PKG_NAME}-env-activate.sh $PREFIX/etc/conda/activate.d
-mv ${PKG_NAME}-env-deactivate.sh $PREFIX/etc/conda/deactivate.d
+## add the /util directory to the PATH inside this conda env
+## http://conda.pydata.org/docs/using/envs.html#saved-environment-variables
+#mkdir -p "$PREFIX"/etc/conda/activate.d
+#echo >"$PREFIX"/etc/conda/activate.d/"$PKG_NAME"-env-activate.sh '#!/usr/bin/env bash
+#
+#export PRE_'"$PKG_NAME"'_PATH="$PATH"
+#export PATH="$CONDA_PREFIX/util:$PATH"
+#'
+#
+#mkdir -p "$PREFIX"/etc/conda/deactivate.d
+#echo >"$PREFIX"/etc/conda/deactivate.d/"$PKG_NAME"-env-deactivate.sh '#!/usr/bin/env bash
+#
+#export PATH="$PRE_'"$PKG_NAME"'_PATH"
+#unset PRE_'"$PKG_NAME"'_PATH
+#'
